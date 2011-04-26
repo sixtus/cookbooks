8b45605a87f3d9a9a5709c1d8f71c4fe55188506
Benedikt Böhm <bb@xnull.de>
2011-04-26 07:52:25 +0200

    sudo: add support for custom rules

---
+++ cookbooks/sudo/definitions/rule.rb
@@ -0,0 +1,9 @@
+define :sudo_rule,
+  :host => "ALL",
+  :runas => "ALL" do
+  if params[:command].kind_of?(String)
+    params[:command] = [params[:command]]
+  end
+  params[:command] = params[:command].join(", ")
+  node.set[:sudo][:rules][params[:name]] = params
+end
