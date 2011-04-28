6b19186eb9b45fd2ec1516e10e737fa5851faf91
Benedikt Böhm <bb@xnull.de>
2011-04-28 15:36:01 +0200

    mongodb: keywrds newer versions

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -1,4 +1,4 @@
-portage_package_keywords "=dev-db/mongodb-1.8.1"
+portage_package_keywords "~dev-db/mongodb-1.8.1"
 
 portage_package_use "dev-db/mongodb" do
   use %w(v8)
@@ -7,7 +7,7 @@ end
 package "dev-db/mongodb"
 
 if tagged?("nagios-client")
-  portage_package_keywords "=dev-python/pymongo-1.9"
+  portage_package_keywords "~dev-python/pymongo-1.10.1"
   package "dev-python/pymongo"
 
   nagios_plugin "mongodb" do
