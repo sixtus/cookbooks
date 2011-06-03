b146b794e5546b903550915f0ef2f79252260b36
Benedikt Böhm <bb@xnull.de>
2011-06-03 20:13:16 +0200

    gitosis: remove obsolete gitosis_repo resource

--- cookbooks/gitosis/recipes/default.rb
+++ cookbooks/gitosis/recipes/default.rb
@@ -6,10 +6,3 @@ execute "gitosis-init" do
   command "sudo -H -u git gitosis-init < /root/.ssh/id_rsa.pub"
   creates "/var/spool/gitosis/.gitosis.conf"
 end
-
-cookbook_file "/usr/bin/git-init-bare-empty" do
-  source "git-init-bare-empty.sh"
-  owner "root"
-  group "root"
-  mode "0755"
-end
