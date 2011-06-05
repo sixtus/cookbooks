e4146286c682478935f0057c6cd9ba87a7a0be4e
Benedikt Böhm <bb@xnull.de>
2011-06-05 16:32:58 +0200

    add spawn-fcgi cookbook

---
+++ cookbooks/spawn-fcgi/definitions/instance.rb
@@ -0,0 +1,36 @@
+define :spawn_fcgi do
+  p = {
+    :children => 1,
+    :chdir => '',
+  }.merge(params.symbolize_keys)
+
+  p[:socket] = {
+    :path => "/var/run/spawn-fcgi/#{params[:name]}.sock",
+    :address => "127.0.0.1",
+    :user => "nobody",
+    :group => "nobody",
+    :mode => "0660",
+  }.merge(p[:socket].symbolize_keys)
+
+  include_recipe "spawn-fcgi"
+
+  name = p[:name]
+
+  link "/etc/init.d/spawn-fcgi.#{name}" do
+    to "/etc/init.d/spawn-fcgi"
+  end
+
+  template "/etc/conf.d/spawn-fcgi.#{name}" do
+    source "spawn-fcgi.confd"
+    cookbook "spawn-fcgi"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables p
+    notifies :restart, "service[spawn-fcgi.#{name}]"
+  end
+
+  service "spawn-fcgi.#{name}" do
+    action [:enable, :start]
+  end
+end
