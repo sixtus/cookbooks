df55577f8f794a8b5f7fd9b620920351d7756374
Benedikt Böhm <bb@xnull.de>
2011-06-06 11:31:46 +0200

    backup: fix detection of nodes that actually have backups configured

--- cookbooks/backup/recipes/default.rb
+++ cookbooks/backup/recipes/default.rb
@@ -6,7 +6,9 @@ ssh_chroot_sftp "backup" do
 end
 
 node.run_state[:nodes].each do |n|
-  next unless n[:backup] and n[:backup][:configs]
+  next unless n[:backup]
+  next unless n[:backup][:configs]
+  next if n[:backup][:configs].empty?
 
   directory "/backup/#{n[:fqdn]}" do
     owner "backup"
