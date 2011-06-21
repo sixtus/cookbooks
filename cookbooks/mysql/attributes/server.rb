730ce195219ddc400490cebb492b7eae77468183
Daniel Bößwetter <daniel.boesswetter@sponsorpay.com>
2011-06-21 11:56:19 +0200

    mysql: add slave_transaction_retries attribute

--- cookbooks/mysql/attributes/server.rb
+++ cookbooks/mysql/attributes/server.rb
@@ -26,6 +26,7 @@ default[:mysql][:server][:expire_logs_days] = 14
 default[:mysql][:server][:log_slave_updates] = false
 default[:mysql][:server][:replicate_do_db] = false
 default[:mysql][:server][:replicate_do_table] = false
+default[:mysql][:server][:slave_transaction_retries] = 10
 
 if node[:mysql][:server][:slave_enabled]
   default[:mysql][:server][:log_bin] = true
