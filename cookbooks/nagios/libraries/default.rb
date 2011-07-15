35e52c287b35b5711a2472f1e57353b6d0b5c7d7
Benedikt Böhm <bb@xnull.de>
2011-07-15 13:26:30 +0200

    nagios: remove obsolete helper method

--- cookbooks/nagios/libraries/default.rb
+++
@@ -1,7 +0,0 @@
-def nagios_service_hosts(hosts, service)
-  service_hosts = []
-  hosts.each do |n|
-    service_hosts << n[:fqdn] if n[:tags].include?("nagios-#{service}")
-  end
-  service_hosts.join(",")
-end
