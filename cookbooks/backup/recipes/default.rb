60e3b0b3b910f5fdd716006d08920b92a19fd084
Benedikt Böhm <bb@xnull.de>
2011-07-18 21:06:14 +0200

    backup: simplify backup accounts and sftp chroot

--- cookbooks/backup/recipes/default.rb
+++ cookbooks/backup/recipes/default.rb
@@ -9,41 +9,15 @@ node.run_state[:users].select do |u|
 end.each do |u|
   account_from_databag u[:id] do
     gid "backup"
-    home "/backup"
-    home_owner "root"
-    home_group "root"
-    home_mode "0755"
+    home "/backup/#{u[:id]}"
+    home_owner u[:id]
+    home_group "backup"
+    home_mode "0700"
   end
 
   group "backup" do
     members u[:id]
     append true
-  end
-
-  # special case for user backup:
-  #
-  # create directories for all hosts
-  # that want to do backup via duply
-  if u[:id] == "backup"
-    node.run_state[:nodes].each do |n|
-      next unless n[:backup]
-      next unless n[:backup][:configs]
-      next if n[:backup][:configs].empty?
-
-      directory "/backup/#{n[:fqdn]}" do
-        owner "backup"
-        group "backup"
-        mode "0700"
-      end
-    end
-  elsif u[:backup_paths]
-    u[:backup_paths].each do |p|
-      directory "/backup/#{p}" do
-        owner u[:id]
-        group "backup"
-        mode "0700"
-      end
-    end
   end
 
   u[:chroot] = true if u[:chroot].nil?
