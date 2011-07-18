ac624c2a7065be5a076897212ca7920fc4760d22
Benedikt Böhm <bb@xnull.de>
2011-07-18 17:17:05 +0200

    account: add magic for accounts created from tags and host lists

--- cookbooks/account/recipes/default.rb
+++ cookbooks/account/recipes/default.rb
@@ -37,11 +37,29 @@ directory "/root/.dotfiles" do
   recursive true
 end
 
+# create hostmaster accounts
 query = Proc.new do |u|
   u[:tags] and u[:tags].include?("hostmaster")
 end
 
 accounts_from_databag "hostmasters" do
   groups %w(cron portage wheel)
+  query query
+end
+
+# create node specific accounts
+query = Proc.new do |u|
+  u[:hosts] and u[:hosts].include?(node[:fqdn])
+end
+
+accounts_from_databag "node-specific" do
+  query query
+end
+
+query = Proc.new do |u|
+  u[:tags] and not (node[:account][:tags] & u[:tags]).empty?
+end
+
+accounts_from_databag "node-tags" do
   query query
 end
