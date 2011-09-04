789704146d251306aa0fa3211ca2ffcd68fdcfc3
Benedikt Böhm <bb@xnull.de>
2011-09-04 17:51:30 +0200

    openssl: make sure invalid symlinks in /etc/ssl/certs are being removed

--- cookbooks/openssl/recipes/default.rb
+++ cookbooks/openssl/recipes/default.rb
@@ -1,10 +1,33 @@
 package "dev-libs/openssl"
+package "app-misc/ca-certificates"
 
 template "/etc/ssl/openssl.cnf" do
   source "openssl.cnf.erb"
   owner "root"
   group "root"
   mode "0644"
+end
+
+ruby_block "cleanup-ca-certificates" do
+  block do
+    Find.find('/etc/ssl/certs') do |path|
+      if File.symlink?(path) and not File.exist?(path)
+        File.unlink(path)
+      end
+    end
+  end
+
+  only_if do
+    result = false
+
+    Find.find('/etc/ssl/certs') do |path|
+      if File.symlink?(path) and not File.exist?(path)
+        result = true
+      end
+    end
+
+    result
+  end
 end
 
 nagios_plugin "check_ssl_server"
