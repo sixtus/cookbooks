e8d53c49e7257d715f44d6d472c55b538cf651e8
Benedikt Böhm <bb@xnull.de>
2011-11-06 16:10:23 +0100

    smart: fix VirtualBox detection

--- cookbooks/smart/attributes/default.rb
+++ cookbooks/smart/attributes/default.rb
@@ -1,8 +1,9 @@
 default[:smart][:devices] = node[:block_device].select do |name, dev|
-  dev[:vendor] == 'ATA' and
-  dev[:model] not in [
+  dev[:vendor] == 'ATA'
+end.reject do |name, dev|
+  [
     'VBOX HARDDISK',
-  ]
+  ].include?(dev[:model])
 end.map do |name, dev|
   "/dev/#{name}"
 end
