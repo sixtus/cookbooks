7ef2a160dd62499f5df862a3cf783fc267ff6b94
Benedikt Böhm <bb@xnull.de>
2011-11-26 14:08:50 +0100

    add jenkins cookbook

---
+++ cookbooks/jenkins/recipes/default.rb
@@ -0,0 +1,12 @@
+package "dev-util/jenkins-bin"
+
+execute "jenkins-ssh-key" do
+  command "ssh-keygen -f /var/lib/jenkins/.ssh/id_rsa -N '' -C jenkins@#{node[:fqdn]}"
+  creates "/var/lib/jenkins/.ssh/id_rsa"
+  user "jenkins"
+  group "jenkins"
+end
+
+service "jenkins" do
+  action [:enable, :start]
+end
