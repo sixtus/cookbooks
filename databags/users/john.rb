a15ea88a9f58dbec602b007c3e8aeed1752d994a
Benedikt Böhm <bb@xnull.de>
2011-11-26 11:21:32 +0100

    remove example databag. a real databag will be generated on server bootstrap

--- databags/users/john.rb
+++
@@ -1,7 +0,0 @@
-self[:comment] = "John Doe"
-self[:email] = "john@example.com"
-self[:groups] = [ "hostmaster" ]
-self[:shell] = "/bin/bash"
-self[:authorized_keys] = [
-  'ssh-rsa ... john@linux'
-]
