48703bb4fb0525c93e1503771305dbf39bd9d591
Benedikt Böhm <bb@xnull.de>
2011-12-06 08:27:52 +0100

    nginx: create fastcgi_params symlink for backwards compatibility

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -85,8 +85,8 @@ link "/etc/nginx/fastcgi.conf" do
   to "/etc/nginx/modules/fastcgi.conf"
 end
 
-file "/etc/nginx/fastcgi_params" do
-  action :delete
+link "/etc/nginx/fastcgi_params" do
+  to "/etc/nginx/modules/fastcgi.conf"
 end
 
 nginx_server "status" do
