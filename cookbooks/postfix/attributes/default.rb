c2d57d44d4d2768221e512f9f0cc6ac00fc186c7
Benedikt Böhm <bb@xnull.de>
2011-12-09 10:43:35 +0100

    postfix: add attribute for message_size_limit

--- cookbooks/postfix/attributes/default.rb
+++ cookbooks/postfix/attributes/default.rb
@@ -2,6 +2,7 @@ default[:skip][:postfix_satelite] = false
 
 default[:postfix][:use_flags] = []
 
+default[:postfix][:message_size_limit] = 10
 default[:postfix][:mynetworks] = []
 default[:postfix][:relayhost] = "mail.#{node[:domain]}"
 default[:postfix][:rbl_servers] = %w(zen.spamhaus.org bl.spamcop.net)
--- cookbooks/postfix/recipes/default.rb
+++ cookbooks/postfix/recipes/default.rb
@@ -71,7 +71,8 @@ postconf "base" do
   set :myhostname => node[:fqdn],
       :mydomain => node[:domain],
       :mynetworks_style => "host",
-      :inet_protocols => "ipv4#{ipv6_str}"
+      :inet_protocols => "ipv4#{ipv6_str}",
+      :message_size_limit => node[:postfix][:message_size_limit].to_i * 1024 * 1024
 end
 
 postmaster "smtp" do
