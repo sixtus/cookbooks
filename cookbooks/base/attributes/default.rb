9bd189339f959124fb8691176a249456ee691088
Benedikt Böhm <bb@xnull.de>
2011-12-21 10:30:30 +0100

    base: allow memory overcommit. too much stuff breaks with no overcommit

--- cookbooks/base/attributes/default.rb
+++ cookbooks/base/attributes/default.rb
@@ -27,7 +27,7 @@ default[:sysctl][:net][:netfilter][:nf_conntrack_max] = 262144
 default[:sysctl][:kernel][:sysrq] = 1
 default[:sysctl][:kernel][:panic] = 60
 default[:sysctl][:vm][:overcommit_ratio] = 95
-default[:sysctl][:vm][:overcommit_memory] = 2
+default[:sysctl][:vm][:overcommit_memory] = 0
 
 # shared memory sizes
 default_unless[:sysctl][:kernel][:shmall] = 2*1024*1024 #pages
