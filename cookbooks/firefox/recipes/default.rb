a453451f93adc1bd7d4c72eb189deec3fd2c6906
Benedikt Böhm <bb@xnull.de>
2011-12-21 10:52:06 +0100

    add cookbooks for xvfb and firefox

---
+++ cookbooks/firefox/recipes/default.rb
@@ -0,0 +1,23 @@
+include_recipe "xvfb"
+
+portage_package_use "dev-libs/libxml2" do
+  use %w(python)
+end
+
+portage_package_use "media-libs/libpng" do
+  use %w(apng)
+end
+
+portage_package_use "x11-libs/cairo" do
+  use %w(X)
+end
+
+portage_package_use "x11-libs/pango" do
+  use %w(X)
+end
+
+portage_package_use "x11-libs/gdk-pixbuf" do
+  use %w(X)
+end
+
+package "www-client/firefox"
---
+++ cookbooks/xvfb/recipes/default.rb
@@ -0,0 +1,11 @@
+layman_overlay "zentoo-x11"
+
+portage_package_use "x11-base/xorg-server" do
+  use %w(-xorg minimal xvfb)
+end
+
+portage_package_use "x11-apps/xinit" do
+  use %w(minimal)
+end
+
+package "x11-base/xorg-server"
