923228ed7740eabd0eac68fbe5963fa85be1f0be
Benedikt Böhm <bb@xnull.de>
2012-01-20 20:01:52 +0100

    update documentation & metadata

--- cookbooks/rvm/metadata.rb
+++ cookbooks/rvm/metadata.rb
@@ -1,1 +1,8 @@
+description "This cookbook installs the Ruby Version Manager for user accounts"
+
+maintainer "Benedikt Böhm"
+maintainer_email "bb@xnull.de"
+license "Apache v2.0"
+
 depends "account"
+depends "portage"
