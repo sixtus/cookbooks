3cea8c87c8925551539867bdd9ec586b7f7ea9df
Benedikt Böhm <bb@xnull.de>
2012-02-09 20:44:45 +0100

    nginx: add fastcgi_buffers attribute

--- cookbooks/nginx/attributes/default.rb
+++ cookbooks/nginx/attributes/default.rb
@@ -3,5 +3,6 @@ default[:nginx][:use_flags] = []
 default[:nginx][:worker_processes] = "4"
 default[:nginx][:worker_connections] = "1024"
 default[:nginx][:client_max_body_size] = "10M"
+default[:nginx][:fastcgi_buffers] = 256
 
 default[:nginx][:php][:max_children] = "4"
