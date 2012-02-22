ea9345d9c6f5a92fcb83564b534e6a572176ddcd
Benedikt Böhm <bb@xnull.de>
2012-02-22 11:39:13 +0100

    rvm: try to solve NameError: uninitialized constant ChefUtils::Account

--- cookbooks/rvm/providers/instance.rb
+++ cookbooks/rvm/providers/instance.rb
@@ -1,4 +1,19 @@
-include ChefUtils::RVM
+include ChefUtils::Account
+
+def infer_vars(user, version = nil)
+  user = get_user(user)
+  path = user[:name] == "root" ? "/usr/local/rvm" : "#{user[:dir]}/.rvm"
+  rvmrc = user[:name] == "root" ? "/etc/rvmrc" : "#{user[:dir]}/.rvmrc"
+
+  return {
+    :user => user[:name],
+    :group => user[:group][:name],
+    :homedir => user[:dir],
+    :path => path,
+    :rvmrc => rvmrc,
+    :version => version,
+  }
+end
 
 action :create do
   rvm = infer_vars(new_resource.name, new_resource.version)
