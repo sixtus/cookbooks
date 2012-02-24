51d455284f282174ee55530ebb1c256ac768a488
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-02-24 11:35:50 +0100

    vim: improve vimrc with pathogen and modules

--- cookbooks/vim/attributes/default.rb
+++ cookbooks/vim/attributes/default.rb
@@ -4,3 +4,10 @@ default[:vim][:rcfile] = case node[:platform]
                          when "mac_os_x"
                            "#{node[:homedir]}/.vimrc"
                          end
+
+default[:vim][:rcdir] = case node[:platform]
+                        when "gentoo"
+                          "/etc/vim"
+                        when "mac_os_x"
+                          "#{node[:homedir]}/.vim"
+                        end
--- cookbooks/vim/recipes/default.rb
+++ cookbooks/vim/recipes/default.rb
@@ -2,7 +2,43 @@ unless platform?("mac_os_x")
   package "app-editors/vim"
 end
 
-cookbook_file node[:vim][:rcfile] do
+directory node[:vim][:rcdir] do
+  mode "0755"
+end
+
+if solo?
+  directory "#{node[:vim][:rcdir]}/autoload" do
+    mode "0755"
+  end
+
+  cookbook_file "#{node[:vim][:rcdir]}/autoload/pathogen.vim" do
+    source "pathogen.vim"
+    mode "0644"
+  end
+
+  directory "#{node[:vim][:rcdir]}/bundle" do
+    mode "0755"
+  end
+
+  {
+    :auto_mkdir => "https://github.com/DataWraith/auto_mkdir",
+    :closetag => "https://github.com/vim-scripts/closetag.vim",
+    :coffee_script => "https://github.com/kchmck/vim-coffee-script",
+    :endwise => "https://github.com/tpope/vim-endwise",
+    :haml => "https://github.com/tpope/vim-haml",
+    :javascript => "https://github.com/pangloss/vim-javascript",
+    :matchit => "https://github.com/vim-scripts/matchit.zip",
+    :ruby => "https://github.com/vim-ruby/vim-ruby",
+  }.each do |name, repo|
+    git "#{node[:vim][:rcdir]}/bundle/#{name}" do
+      repository repo
+      reference "master"
+      action :sync
+    end
+  end
+end
+
+template node[:vim][:rcfile] do
   source "vimrc"
   mode "0644"
 end
