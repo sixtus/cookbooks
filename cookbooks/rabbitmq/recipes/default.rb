33306cf30d0b3b8afadde100ad7736964be0ba39
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-03-08 16:03:54 +0100

    rabbitmq: create log directory if it does not exist

--- cookbooks/rabbitmq/recipes/default.rb
+++ cookbooks/rabbitmq/recipes/default.rb
@@ -1,5 +1,11 @@
 package "net-misc/rabbitmq-server"
 
+directory "/var/log/rabbitmq" do
+  owner "rabbitmq"
+  group "rabbitmq"
+  mode "0770"
+end
+
 service "rabbitmq" do
   action [:enable, :start]
 end
