7cd39c2cbcc32b23b93bef78929ce643bca9212c
Benedikt Böhm <bb@xnull.de>
2012-03-19 13:27:55 +0100

    base: add nagios check for lib_users

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -139,6 +139,23 @@ if tagged?("nagios-client")
     servicegroups "system"
   end
 
+  sudo_rule "nagios-lib_users" do
+    user "nagios"
+    runas "root"
+    command "NOPASSWD: /usr/bin/lib_users"
+  end
+
+  nagios_plugin "check_lib_users"
+
+  nrpe_command "check_lib_users" do
+    command "/usr/lib/nagios/plugins/check_lib_users"
+  end
+
+  nagios_service "LIB-USERS" do
+    check_command "check_nrpe!check_lib_users"
+    servicegroups "system"
+  end
+
   if node[:virtualization][:role] == "host"
     nagios_plugin "check_raid"
 
