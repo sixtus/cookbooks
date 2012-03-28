1112e1e124a62ca5e1045f5b9b411b12e35315a8
Benedikt Böhm <bb@xnull.de>
2012-03-28 14:10:36 +0200

    nginx: add a bunch of timeout attributes

--- cookbooks/nginx/attributes/default.rb
+++ cookbooks/nginx/attributes/default.rb
@@ -2,7 +2,13 @@ default[:nginx][:use_flags] = []
 
 default[:nginx][:worker_processes] = "4"
 default[:nginx][:worker_connections] = "1024"
+
+default[:nginx][:client_body_timeout] = 60
+default[:nginx][:client_header_timeout] = 60
 default[:nginx][:client_max_body_size] = "10M"
+default[:nginx][:keepalive_timeout] = 75
+default[:nginx][:send_timeout] = 60
+
 default[:nginx][:fastcgi_buffers] = 256
 
 default[:nginx][:php][:max_children] = "4"
