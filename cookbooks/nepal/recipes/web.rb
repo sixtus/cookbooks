9600c4a15afd92a5f945f5e2912ad8dbc90b3f51
Benedikt Böhm <bb@xnull.de>
2012-03-30 11:32:18 +0200

    nepal: do not setup standard apache php recipe

--- cookbooks/nepal/recipes/web.rb
+++ cookbooks/nepal/recipes/web.rb
@@ -23,8 +23,13 @@ execute "/usr/bin/nepalc generate_apache_conf" do
   creates "/srv/system/etc/apache/listen.conf"
 end
 
-include_recipe "apache::php"
+include_recipe "apache::fastcgi"
 include_recipe "apache::wsgi"
+
+file "/etc/apache2/modules.d/70_php_fpm.conf" do
+  action :delete
+  notifies :reload, "service[apache2]"
+end
 
 apache_vhost "nepal" do
   template "web/apache.conf"
