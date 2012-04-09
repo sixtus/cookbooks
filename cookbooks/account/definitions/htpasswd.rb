8977d1834d6bf586619ec59b4d0eecfed8b7a6c7
Benedikt Böhm <bb@xnull.de>
2012-04-09 09:30:29 +0200

    account: add support for htpasswd from tags or simple user list

--- cookbooks/account/definitions/htpasswd.rb
+++ cookbooks/account/definitions/htpasswd.rb
@@ -1,5 +1,21 @@
 define :htpasswd_from_databag, :owner => "root", :group => "root", :mode => "0440" do
-  content = node.run_state[:users].select(&params[:query]).select do |user|
+  if params[:query]
+    query = params[:query]
+  elsif params[:tags]
+    tags = [params[:tags]].flatten
+    query = Proc.new do |u|
+      u[:tags] and (u[:tags] & tags).any?
+    end
+  elsif params[:users]
+    users = [params[:users]].flatten
+    query = Proc.new do |u|
+      users.include?(u[:id])
+    end
+  else
+    raise "no query or options specified"
+  end
+
+  content = node.run_state[:users].select(&query).select do |user|
     user[:password]
   end.map do |user|
     "#{user[:id]}:#{user[:password]}"
