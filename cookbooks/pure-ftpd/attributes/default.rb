0321c0fe19dc6c45e75cc8d108621c369429e88c
Benedikt Böhm <bb@xnull.de>
2012-04-20 20:13:29 +0200

    pure-ftpd: remove -x and -Z from default options

--- cookbooks/pure-ftpd/attributes/default.rb
+++ cookbooks/pure-ftpd/attributes/default.rb
@@ -3,4 +3,4 @@ default[:pureftpd][:connections] = 30
 default[:pureftpd][:ip_connections] = 10
 default[:pureftpd][:idle_timeout] = 5
 default[:pureftpd][:auth] = []
-default[:pureftpd][:options] = "-A -E -x -j -Z"
+default[:pureftpd][:options] = "-A -E -j"
