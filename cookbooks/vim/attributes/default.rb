9a67c772f95d9c5a99a9c7c875bc0749694d432e
Benedikt Böhm <bb@xnull.de>
2012-04-25 23:07:07 +0200

    add macvim and some vim foo

--- cookbooks/vim/attributes/default.rb
+++ cookbooks/vim/attributes/default.rb
@@ -11,3 +11,16 @@ default[:vim][:rcdir] = case node[:platform]
                         when "mac_os_x"
                           "#{node[:homedir]}/.vim"
                         end
+
+default[:vim][:plugins] = {
+  :auto_mkdir => "https://github.com/DataWraith/auto_mkdir",
+  :closetag => "https://github.com/vim-scripts/closetag.vim",
+  :coffee_script => "https://github.com/kchmck/vim-coffee-script",
+  :endwise => "https://github.com/tpope/vim-endwise",
+  :haml => "https://github.com/tpope/vim-haml",
+  :javascript => "https://github.com/pangloss/vim-javascript",
+  :matchit => "https://github.com/vim-scripts/matchit.zip",
+  :ruby => "https://github.com/vim-ruby/vim-ruby",
+  :ctrlp => "https://github.com/kien/ctrlp.vim.git",
+  :"vim-rails" => "https://github.com/tpope/vim-rails.git",
+}
--- cookbooks/vim/recipes/default.rb
+++ cookbooks/vim/recipes/default.rb
@@ -20,16 +20,8 @@ if solo? and not root?
     mode "0755"
   end
 
-  {
-    :auto_mkdir => "https://github.com/DataWraith/auto_mkdir",
-    :closetag => "https://github.com/vim-scripts/closetag.vim",
-    :coffee_script => "https://github.com/kchmck/vim-coffee-script",
-    :endwise => "https://github.com/tpope/vim-endwise",
-    :haml => "https://github.com/tpope/vim-haml",
-    :javascript => "https://github.com/pangloss/vim-javascript",
-    :matchit => "https://github.com/vim-scripts/matchit.zip",
-    :ruby => "https://github.com/vim-ruby/vim-ruby",
-  }.each do |name, repo|
+  node[:vim][:plugins].each do |name, repo|
+    next unless repo
     git "#{node[:vim][:rcdir]}/bundle/#{name}" do
       repository repo
       reference "master"
