7fc4e4394acffcfdfe3b3d731ab06b8d7c40561e
Benedikt Böhm <bb@xnull.de>
2012-04-28 21:55:45 +0200

    tmux: add support for overridable config

--- cookbooks/tmux/recipes/default.rb
+++ cookbooks/tmux/recipes/default.rb
@@ -3,7 +3,15 @@ package value_for_platform(
   "mac_os_x" => {"default" => "tmux"}
 )
 
-cookbook_file node[:tmux][:configfile] do
+template node[:tmux][:configfile] do
   source "tmux.conf"
   mode "0644"
+end
+
+if solo? and not root?
+  overridable_template "#{node[:homedir]}/.tmux.conf.local" do
+    source "tmux.conf.local"
+    namespace :user
+    instance node[:current_user]
+  end
 end
