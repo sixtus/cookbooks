6178eee978ed533be01c71713123474b39d6dc27
Benedikt Böhm <bb@xnull.de>
2012-05-08 08:57:24 +0200

    add R cookbook

---
+++ cookbooks/R/metadata.rb
@@ -0,0 +1,5 @@
+description "Install and configure R - the project for statistical computing"
+
+maintainer "Benedikt Böhm"
+maintainer_email "bb@xnull.de"
+license "Apache v2.0"
---
+++ cookbooks/R/recipes/default.rb
@@ -0,0 +1,5 @@
+portage_package_use "x11-libs/cairo" do
+  use %w(X)
+end
+
+package "dev-lang/R"
