f9cdd151fc6dfb83503b7dfd3ef85b01bee85d49
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-05-15 16:02:12 +0200

    account: add support for vagrant insecure public key

--- cookbooks/account/libraries/authorized_keys.rb
+++ cookbooks/account/libraries/authorized_keys.rb
@@ -8,7 +8,11 @@ module ChefUtils
         not u[:authorized_keys].empty?
       end.map do |u|
         u[:authorized_keys]
-      end.flatten
+      end.flatten.tap do |keys|
+        if node[:authorize_vagrant_public_key]
+          keys << "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+        end
+      end
     end
   end
 end
