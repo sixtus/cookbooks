408d35d89737535551e4569a5c4506e43326e743
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-05-22 11:46:33 +0200

    hwraid: add support for megaraid

--- cookbooks/hwraid/recipes/default.rb
+++ cookbooks/hwraid/recipes/default.rb
@@ -2,4 +2,6 @@ devices = File.new("/proc/bus/pci/devices").readlines
 
 if devices.grep(/aacraid/).any?
   include_recipe "hwraid::aac"
+elsif devices.grep(/megaraid_sas/).any?
+  include_recipe "hwraid::megaraid"
 end
---
+++ cookbooks/hwraid/recipes/megaraid.rb
@@ -0,0 +1,7 @@
+package "sys-block/megacli"
+
+sudo_rule "nagios-megacli" do
+  user "nagios"
+  runas "root"
+  command "NOPASSWD: /opt/bin/MegaCli -PDList -aALL -NoLog"
+end
