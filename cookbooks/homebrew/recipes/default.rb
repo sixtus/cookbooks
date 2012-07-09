58ec4d488e2172ce55275f60fe833225d2c44508
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-07-09 21:04:37 +0200

    homebrew: only change origin if there is a repository

--- cookbooks/homebrew/recipes/default.rb
+++ cookbooks/homebrew/recipes/default.rb
@@ -2,6 +2,7 @@ execute "set homebrew origin" do
   command "git remote set-url origin #{node[:homebrew][:repo]}"
   cwd "/usr/local/"
   only_if do
+    File.exist?("/usr/local/.git") and
     %x(GIT_DIR=/usr/local/.git git remote show -n origin | grep Fetch | awk '{print $3}').chomp != node[:homebrew][:repo]
   end
 end
