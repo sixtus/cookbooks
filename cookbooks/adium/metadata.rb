bbaf12778ebe8ae9579ed2e2263d4f56579de01b
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-07-13 08:48:24 +0200

    add adium cookbook

---
+++ cookbooks/adium/metadata.rb
@@ -0,0 +1,9 @@
+description "Install Adium"
+
+maintainer "Benedikt Böhm"
+maintainer_email "bb@xnull.de"
+license "Apache v2.0"
+
+supports "mac_os_x"
+
+depends "mac"
---
+++ cookbooks/adium/recipes/default.rb
@@ -0,0 +1,5 @@
+if platform?("mac_os_x")
+  mac_package "Adium" do
+    source "http://download.adium.im/Adium_1.5.1.dmg"
+  end
+end
