c344fa415bfaa61981a24a036d7b9ce33cd64f61
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-08-05 16:42:13 +0200

    redis: add nagios check

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -3,3 +3,13 @@ package "dev-db/redis"
 service "redis" do
   action [:enable, :start]
 end
+
+if tagged?("nagios-client")
+  nrpe_command "check_redis" do
+    command "/usr/lib/nagios/plugins/check_pidfile /var/run/redis/redis.pid /usr/sbin/redis-server"
+  end
+
+  nagios_service "REDIS" do
+    check_command "check_nrpe!check_redis"
+  end
+end
