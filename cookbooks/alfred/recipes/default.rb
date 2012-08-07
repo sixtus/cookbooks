7694cad0c52ff0411c397600ee5a17d43344b8f9
Mike Szörnyi <mike.szoernyi@madvertise.de>
2012-08-07 16:07:14 +0200

    alfred: update download link

--- cookbooks/alfred/recipes/default.rb
+++ cookbooks/alfred/recipes/default.rb
@@ -1,6 +1,6 @@
 if platform?("mac_os_x")
   mac_package "Alfred" do
-    source "http://rwc.cachefly.net/alfred_1.2_220.dmg"
+    source "http://media.alfredapp.com/alfred_1.3_249.dmg"
     volumes_dir "Alfred.app"
   end
 end
