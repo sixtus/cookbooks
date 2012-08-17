018e79db9002fcc5e50cbc0014f37ed2c15a8db3
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-08-17 10:17:33 +0200

    R: add some use flags for new R version

--- cookbooks/R/recipes/default.rb
+++ cookbooks/R/recipes/default.rb
@@ -1,5 +1,13 @@
+portage_package_use "sys-libs/zlib" do
+  use %w(minizip)
+end
+
 portage_package_use "x11-libs/cairo" do
   use %w(X)
+end
+
+portage_package_use "dev-lang/R" do
+  use %w(cairo)
 end
 
 package "dev-lang/R"
