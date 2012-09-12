1d86595f142adc28f29e90f9ea5fbb71b2710994
Benedikt Böhm <benedikt.boehm@madvertise.com>
2012-09-12 12:58:25 +0200

    git: only deploy hub on solo mode

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -3,12 +3,6 @@ package value_for_platform(
   "mac_os_x" => {"default" => "git"}
 )
 
-remote_file "#{node[:homedir]}/bin/hub" do
-  source "http://defunkt.io/hub/standalone"
-  checksum "d1b6ced5c012d924d226bb14631fe58218ed0ad9561b181aff4c1a1d97996c29"
-  mode "0755"
-end
-
 template node[:git][:rcfile] do
   source "gitconfig"
   mode "0644"
@@ -20,6 +14,12 @@ cookbook_file node[:git][:exfile] do
 end
 
 if solo? and not root?
+  remote_file "#{node[:homedir]}/bin/hub" do
+    source "http://defunkt.io/hub/standalone"
+    checksum "d1b6ced5c012d924d226bb14631fe58218ed0ad9561b181aff4c1a1d97996c29"
+    mode "0755"
+  end
+
   overridable_template "#{node[:homedir]}/.gitconfig.local" do
     source "gitconfig.local"
     namespace :user
