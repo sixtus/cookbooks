3ffba6788467ef1e1f3677115ed12afbe44360d7
Benedikt Böhm <bb@xnull.de>
2012-09-18 11:24:22 +0200

    mac: update some packages

--- cookbooks/adium/recipes/default.rb
+++ cookbooks/adium/recipes/default.rb
@@ -1,5 +1,5 @@
 if platform?("mac_os_x")
   mac_package "Adium" do
-    source "http://download.adium.im/Adium_1.5.1.dmg"
+    source "http://download.adium.im/Adium_1.5.3.dmg"
   end
 end
--- cookbooks/alfred/recipes/default.rb
+++ cookbooks/alfred/recipes/default.rb
@@ -1,6 +1,6 @@
 if platform?("mac_os_x")
   mac_package "Alfred" do
-    source "http://media.alfredapp.com/alfred_1.3_249.dmg"
+    source "http://cachefly.alfredapp.com/alfred_1.3.1_261.dmg"
     volumes_dir "Alfred.app"
   end
 end
