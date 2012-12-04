38a13be37c49d1bfc025288d00e35ce1111cd73e
Benedikt Böhm <bb@xnull.de>
2012-12-04 23:10:07 +0100

    account: add resource for real database htpasswd files

--- cookbooks/account/definitions/htpasswd.rb
+++ cookbooks/account/definitions/htpasswd.rb
@@ -1,4 +1,4 @@
-define :htpasswd_from_databag, :owner => "root", :group => "root", :mode => "0440", :password_field => :password do
+define :htpasswd_from_users, :owner => "root", :group => "root", :mode => "0440", :password_field => :password do
   if params[:query]
     query = params[:query]
   elsif params[:tags]
@@ -20,6 +20,19 @@ define :htpasswd_from_databag, :owner => "root", :group => "root", :mode => "044
   end.map do |user|
     "#{user[:id]}:#{user[params[:password_field]]}"
   end.join("\n")
+
+  file params[:name] do
+    content content
+    owner params[:owner]
+    group params[:group]
+    mode params[:mode]
+  end
+end
+
+define :htpasswd_from_databag, :owner => "root", :group => "root", :mode => "0440" do
+  content = data_bag_item('htpasswd', params[:bag]).map do |user, password|
+    user == "id" ? nil : "#{user}:#{password}"
+  end.compact.sort.join("\n")
 
   file params[:name] do
     content content
