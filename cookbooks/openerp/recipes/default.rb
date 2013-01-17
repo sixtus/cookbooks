a467d98755eea67de70a17f27756fef4a0416785
Benedikt Böhm <bb@xnull.de>
2013-01-17 09:51:25 +0100

    openerp: add config and passwords

--- cookbooks/openerp/recipes/default.rb
+++ cookbooks/openerp/recipes/default.rb
@@ -1,1 +1,27 @@
 package "app-office/openerp"
+
+group "postgres" do
+  action :modify
+  members "openerp"
+  append true
+end
+
+admin_password = get_password("openerp/admin")
+
+# create user and database with:
+# createuser --createdb --pwprompt --superuser --no-createrole --username postgres openerp
+db_password = get_password("openerp/db")
+
+template "/etc/openerp/openerp.cfg" do
+  source "openerp.cfg"
+  owner "root"
+  group "root"
+  mode "0755"
+  notifies :restart, "service[openerp]"
+  variables :admin_password => admin_password,
+            :db_password => db_password
+end
+
+service "openerp" do
+  action [:enable, :start]
+end
