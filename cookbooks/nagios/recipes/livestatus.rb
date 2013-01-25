a60569c30394f8f9f3f65dbcb783ce5dc6d098ce
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-01-25 09:29:08 +0100

    nagios: use xinetd cookbook

--- cookbooks/nagios/recipes/livestatus.rb
+++ cookbooks/nagios/recipes/livestatus.rb
@@ -1,5 +1,6 @@
+include_recipe "xinetd"
+
 package "net-analyzer/mk-livestatus"
-package "sys-apps/xinetd"
 
 cookbook_file "/etc/xinetd.d/livestatus" do
   source "xinetd.conf"
@@ -7,8 +8,4 @@ cookbook_file "/etc/xinetd.d/livestatus" do
   group "root"
   mode "0644"
   notifies :restart, "service[xinetd]"
-end
-
-service "xinetd" do
-  action [:enable, :start]
 end
