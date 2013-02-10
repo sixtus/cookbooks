744acf4089e6718c08bbc5bbbc2e4b7a251392af
Benedikt Böhm <bb@xnull.de>
2013-02-10 17:14:24 +0100

    set upstream branch to next

--- config/rake.rb
+++ config/rake.rb
@@ -19,7 +19,7 @@ SSL_ORGANIZATIONAL_UNIT_NAME = "Operations"
 SSL_EMAIL_ADDRESS = "hostmaster@example.com"
 
 # The upstream branch to track
-UPSTREAM_BRANCH="master"
+UPSTREAM_BRANCH="next"
 
 # make rake more silent
 RakeFileUtils.verbose_flag = false
