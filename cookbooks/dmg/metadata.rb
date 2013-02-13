ca332fef25551d3161e11ebd318905a5d94d5630
Benedikt Böhm <bb@xnull.de>
2013-02-13 10:59:18 +0100

    remove obsolete dmg cookbook

--- cookbooks/dmg/metadata.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-description "LWRP to install OS X applications from dmgs"
-
-maintainer "Joshua Timberman"
-maintainer_email "cookbooks@housepub.org"
-license "Apache 2.0"
-
-supports "mac_os_x"
