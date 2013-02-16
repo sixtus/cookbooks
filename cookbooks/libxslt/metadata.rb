535d61df3c8a3110fa90de782057ee004a39ffad
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-02-16 09:28:56 +0100

    add libxslt cookbook

---
+++ cookbooks/libxslt/metadata.rb
@@ -0,0 +1,10 @@
+description "Libxslt is the XSLT C library developed for the Gnome project"
+
+maintainer "Benedikt Böhm"
+maintainer_email "benedikt.boehm@madvertise.com"
+license "Apache v2.0"
+
+version "1.0.0"
+
+supports "gentoo"
+supports "mac_os_x"
