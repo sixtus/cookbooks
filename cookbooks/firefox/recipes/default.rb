8fea2ea82a2e006797c839953c94788725b8c7cd
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-02-16 09:34:39 +0100

    firefox: remove obsolete use flags

--- cookbooks/firefox/recipes/default.rb
+++ cookbooks/firefox/recipes/default.rb
@@ -12,15 +12,7 @@ portage_package_use "media-libs/libpng" do
   use %w(apng)
 end
 
-portage_package_use "x11-libs/cairo" do
-  use %w(X)
-end
-
 portage_package_use "x11-libs/pango" do
-  use %w(X)
-end
-
-portage_package_use "x11-libs/gdk-pixbuf" do
   use %w(X)
 end
 
