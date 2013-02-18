856ec51a04216f1a7f27afee4185c50c720838f0
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-02-19 00:10:29 +0100

    chef: only send notifications to airbrake in production and staging

--- cookbooks/chef/templates/default/client.rb
+++ cookbooks/chef/templates/default/client.rb
@@ -13,7 +13,7 @@ chef_server_url    "<%= node[:chef][:client][:server_url] %>"
 file_cache_path    "/var/lib/chef/cache"
 file_backup_path   "/var/lib/chef/backup"
 
-<% if node[:chef][:client][:airbrake][:key] %>
+<% if node[:chef][:client][:airbrake][:key] and ['production', 'staging'].include?(node.chef_environment) %>
 require "airbrake_handler"
 exception_handlers << AirbrakeHandler.new(:api_key => "<%= node[:chef][:client][:airbrake][:key] %>", :notify_host => "<%= node[:chef][:client][:airbrake][:url] %>" )
 <% end %>
