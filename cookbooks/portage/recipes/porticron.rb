7670983d9b20a66f0fa8586c4b18dc5b6878155a
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-02-23 13:06:51 +0000

    portage: remove porticron

--- cookbooks/portage/recipes/porticron.rb
+++ cookbooks/portage/recipes/porticron.rb
@@ -1,19 +1,5 @@
-package "app-portage/porticron"
-
-template "/etc/porticron.conf" do
-  source "porticron.conf"
-  owner "root"
-  group "root"
-  mode "0644"
-  backup 0
-end
-
-# we do this manually, since we cannot depend on the cron cookbook
-%w(hourly weekly).each do |f|
-  directory "/etc/cron.#{f}-#{rrand}" do
-    path "/etc/cron.#{f}"
-    mode "0750"
-  end
+package "app-portage/porticron" do
+  action :remove
 end
 
 file "/etc/cron.daily/porticron" do
@@ -21,13 +7,9 @@ file "/etc/cron.daily/porticron" do
 end
 
 file "/etc/cron.weekly/porticron" do
-  content "#!/bin/sh\n/usr/bin/flock -n -e /var/lock/porticron.cron -c '/usr/sbin/porticron -n'\n"
-  mode "0755"
-  backup 0
+  action :delete
 end
 
 file "/etc/cron.weekly/distfiles" do
-  content "#!/bin/sh\n/usr/bin/flock -n -e /var/lock/distfiles.cron -c '/usr/bin/eclean --quiet distfiles'\n"
-  mode "0755"
-  backup 0
+  action :delete
 end
