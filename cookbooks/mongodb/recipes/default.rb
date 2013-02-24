808ec9345d25e4cc4a42228a8327f824c0d67195
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-02-24 12:52:49 +0000

    mongodb: remove old logrotate config if instance is not deployed

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -6,6 +6,11 @@ package "dev-db/mongodb"
 package "dev-python/pymongo"
 package "dev-ruby/mongo"
 
+file "/etc/logrotate.d/mongodb" do
+  action :delete
+  not_if { node[:mongodb][:instances]["mongodb"] rescue false }
+end
+
 if tagged?("nagios-client")
   nagios_plugin "check_mongodb"
 end
