baa9ab2505a6a8c978972f6e5311b20fe7bda07b
Benedikt Böhm <bb@xnull.de>
2013-02-27 14:53:14 +0100

    Revert "revert all next branches to master after initial next merge"
    This reverts commit 903ef24f20ab4b57072f6807937196606c72e644.

--- tasks/templates/quickstart.sh
+++ tasks/templates/quickstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-wget -q -O quickstart.tar.gz https://github.com/zentoo/quickstart/tarball/master
+wget -q -O quickstart.tar.gz https://github.com/zentoo/quickstart/tarball/next
 tar -xzf quickstart.tar.gz
 cd *-quickstart-*
 exec ./quickstart profiles/<%= args.profile %>.sh
