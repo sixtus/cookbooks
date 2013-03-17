90c2658a22be07740d4faa77d16ff81b36811a3a
Benedikt Böhm <bb@xnull.de>
2013-03-17 19:09:33 +0100

    spawn-fcgi: convert /var/run to /run

--- cookbooks/spawn-fcgi/recipes/default.rb
+++ cookbooks/spawn-fcgi/recipes/default.rb
@@ -1,12 +1,6 @@
 package "www-servers/spawn-fcgi"
 package "www-misc/fcgiwrap"
 
-directory "/var/run/spawn-fcgi" do
-  owner "root"
-  group "root"
-  mode "0755"
-end
-
 file "/etc/conf.d/spawn-fcgi" do
   action :delete
 end
