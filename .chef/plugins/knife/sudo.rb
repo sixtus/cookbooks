1c9e20d0cc791f4eb8ee9d23eb5ac389432976a5
Benedikt Böhm <bb@xnull.de>
2013-03-28 08:39:35 +0100

    add knife sudo plugin

---
+++ .chef/plugins/knife/sudo.rb
@@ -0,0 +1,33 @@
+require 'chef/knife'
+
+module KnifeSudo
+  class Sudo < Chef::Knife
+
+    deps do
+      require 'chef/search/query'
+      require 'chef/knife/ssh'
+      Chef::Knife::Ssh.load_deps
+    end
+
+    banner "knife sudo QUERY COMMAND (options)"
+
+    option :query,
+      :short => "-Q VALUE",
+      :long => "--query VALUE",
+      :description => "Solr query for node search (default: *:*)",
+      :default => "*:*"
+
+    def run
+      if name_args.size == 0
+        $stderr.puts "No command given."
+        exit 1
+      end
+
+      command = "/usr/bin/sudo -i /usr/bin/env LANG=en_US.UTF-8 #{name_args.join(' ')}"
+
+      knife_ssh = Chef::Knife::Ssh.new()
+      knife_ssh.name_args = [config[:query], command]
+      knife_ssh.run
+    end
+  end
+end
