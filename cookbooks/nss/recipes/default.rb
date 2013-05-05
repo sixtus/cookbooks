f1760b86f25679967228a5da3e2772f42bfcf21c
Benedikt Böhm <bb@xnull.de>
2013-05-05 16:20:15 +0200

    nss: add preliminary debian support

--- cookbooks/nss/recipes/default.rb
+++ cookbooks/nss/recipes/default.rb
@@ -1,22 +1,28 @@
-template "/etc/pam.d/system-auth" do
-  source "system-auth.pamd"
-  owner "root"
-  group "root"
-  mode "0644"
-end
-
-template "/etc/pam.d/system-login" do
-  source "system-login.pamd"
-  owner "root"
-  group "root"
-  mode "0644"
-end
-
 template "/etc/nsswitch.conf" do
   source "nsswitch.conf"
   owner "root"
   group "root"
   mode "0644"
+end
+
+case node[:platform]
+when "gentoo"
+  template "/etc/pam.d/system-auth" do
+    source "system-auth.pamd"
+    owner "root"
+    group "root"
+    mode "0644"
+  end
+
+  template "/etc/pam.d/system-login" do
+    source "system-login.pamd"
+    owner "root"
+    group "root"
+    mode "0644"
+  end
+
+when "debian"
+  # do not touch debians pam for now
 end
 
 service "unscd" do
