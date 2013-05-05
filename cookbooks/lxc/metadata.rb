ad56b6195b8ede7c7f40328ed1e950af5bfb3c1c
Benedikt Böhm <bb@xnull.de>
2013-05-05 19:12:21 +0200

    lxc: add debian support

--- cookbooks/lxc/metadata.rb
+++ cookbooks/lxc/metadata.rb
@@ -4,6 +4,7 @@ maintainer "Benedikt Böhm"
 maintainer_email "benedikt.boehm@madvertise.com"
 license "Apache v2.0"
 
-version "0.0.0"
+version "1.0.0"
 
+supports "debian"
 supports "gentoo"
--- cookbooks/lxc/recipes/default.rb
+++ cookbooks/lxc/recipes/default.rb
@@ -1,4 +1,10 @@
-package "app-emulation/lxc"
-package "net-misc/bridge-utils"
+case node[:platform]
+when "gentoo"
+  package "app-emulation/lxc"
+  package "net-misc/bridge-utils"
+
+when "debian"
+  package "lxc"
+end
 
 systemd_unit "lxc@.service"
