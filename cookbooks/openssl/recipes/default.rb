9f9ed905ef09e49018a5f879ad2913ad5fb2fb1d
Benedikt Böhm <bb@xnull.de>
2013-05-05 16:13:42 +0200

    openssl: add debian support

--- cookbooks/openssl/recipes/default.rb
+++ cookbooks/openssl/recipes/default.rb
@@ -3,37 +3,44 @@ when "gentoo"
   package "dev-libs/openssl"
   package "app-misc/ca-certificates"
 
-  template "/etc/ssl/openssl.cnf" do
-    source "openssl.cnf.erb"
-    owner "root"
-    group "root"
-    mode "0644"
-  end
+when "debian"
+  package "openssl"
+  package "ca-certificates"
 
+end
+
+template "/etc/ssl/openssl.cnf" do
+  source "openssl.cnf"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+unless solo?
   ssl_certificate "/etc/ssl/certs/wildcard.#{node[:chef_domain]}" do
     cn "wildcard.#{node[:chef_domain]}"
   end
+end
 
-  ruby_block "cleanup-ca-certificates" do
-    block do
-      Find.find('/etc/ssl/certs') do |path|
-        if File.symlink?(path) and not File.exist?(path)
-          File.unlink(path)
-        end
+ruby_block "cleanup-ca-certificates" do
+  block do
+    Find.find('/etc/ssl/certs') do |path|
+      if File.symlink?(path) and not File.exist?(path)
+        File.unlink(path)
       end
     end
+  end
 
-    only_if do
-      require 'find'
+  only_if do
+    require 'find'
 
-      result = false
+    result = false
 
-      Find.find('/etc/ssl/certs') do |path|
-        result = true if File.symlink?(path) and not File.exist?(path)
-      end
-
-      result
+    Find.find('/etc/ssl/certs') do |path|
+      result = true if File.symlink?(path) and not File.exist?(path)
     end
+
+    result
   end
 end
 
