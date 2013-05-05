1505a1884075fae6ae8f8562f9a2999b5f62dab3
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-05-05 14:24:53 +0200

    base: split attributes in platform sepcific files

---
+++ cookbooks/base/attributes/debian.rb
@@ -0,0 +1,45 @@
+# base packages
+if node[:platform] == "debian"
+  node.set[:packages] = %w(
+    ack
+    apache2-utils
+    atool
+    bash-completion
+    dnsutils
+    bwm-ng
+    colordiff
+    dos2unix
+    ethtool
+    hdparm
+    icu
+    iotop
+    iproute2
+    iptraf-ng
+    keychain
+    less
+    libffi
+    libxml2
+    libxslt
+    libyaml
+    lsof
+    mailx
+    mc
+    mtr
+    ncdu
+    netcat
+    nmap
+    pciutils
+    pwgen
+    pydf
+    strace
+    syslogger
+    sysstat
+    tcpdump
+    tcptraceroute
+    telnet-bsd
+    traceroute
+    whois
+    xz-utils
+    brokenpackage
+  )
+end
