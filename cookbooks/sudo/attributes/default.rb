9acb64e79190c11ed181d7025f5726411f5f5422
Benedikt Böhm <benedikt.boehm@madvertise.com>
2013-05-05 15:26:17 +0200

    sudo: add debian support

--- cookbooks/sudo/attributes/default.rb
+++ cookbooks/sudo/attributes/default.rb
@@ -1,2 +1,8 @@
 default[:sudo][:nopasswd] = "true"
 default[:sudo][:rules] = {}
+default[:sudo][:group] = case node[:platform]
+                         when "debian"
+                           "sudo"
+                         when "gentoo"
+                           "wheel"
+                         end
