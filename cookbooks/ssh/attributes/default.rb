45506407a3d3ff280068e0ae0326dd62ab852e48
Benedikt Böhm <bb@xnull.de>
2013-05-18 14:28:10 +0200

    ssh: enable X11 forwarding by default

--- cookbooks/ssh/attributes/default.rb
+++ cookbooks/ssh/attributes/default.rb
@@ -17,6 +17,6 @@ default[:ssh][:additional_host_keys] = []
 default[:ssh][:server][:password_auth] = "no"
 default[:ssh][:server][:challange_response_auth] = "no"
 default[:ssh][:server][:root_login] = "no"
-default[:ssh][:server][:x11_forwarding] = "no"
+default[:ssh][:server][:x11_forwarding] = "yes"
 default[:ssh][:server][:use_lpk] = "no"
 default[:ssh][:server][:matches] = {}
