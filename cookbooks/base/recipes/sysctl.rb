9ae0aa6250bd93e6c39e740c1f38a335bc5ac65c
Benedikt Böhm <bb@xnull.de>
2013-06-02 10:56:30 +0200

    base: consolidate sysctl for openrc/systemd/lxc

--- cookbooks/base/recipes/sysctl.rb
+++ cookbooks/base/recipes/sysctl.rb
@@ -1,31 +1,31 @@
-if systemd_running?
-  service "systemd-sysctl.service" do
-    action [:enable, :start]
-    only_if { systemd_running? }
-  end
+service "systemd-sysctl.service" do
+  action [:enable, :start]
+  only_if { systemd_running? }
+end
 
-  template "/etc/sysctl.d/base.conf" do
-    owner "root"
-    group "root"
-    mode "0644"
-    source "sysctl.conf"
-    notifies :restart, "service[systemd-sysctl.service]"
+execute "sysctl-reload" do
+  command "/sbin/sysctl -p /etc/sysctl.conf"
+  command "/usr/lib/systemd/systemd-sysctl" if systemd_running?
+  action :nothing
+  not_if do
+    node[:virtualization][:role] == "guest"
   end
-else
-  execute "sysctl-reload" do
-    command "/sbin/sysctl -p /etc/sysctl.conf"
-    action :nothing
-    not_if do
-      node[:virtualization][:system] == "linux-vserver" and
-      node[:virtualization][:role] == "guest"
-    end
-  end
+end
 
-  template "/etc/sysctl.conf" do
-    owner "root"
-    group "root"
-    mode "0644"
-    source "sysctl.conf"
-    notifies :run, "execute[sysctl-reload]" unless systemd_running?
-  end
+link "/etc/sysctl.conf" do
+  to "/etc/sysctl.d/base.conf"
+end
+
+directory "/etc/sysctl.d" do
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+template "/etc/sysctl.d/base.conf" do
+  owner "root"
+  group "root"
+  mode "0644"
+  source "sysctl.conf"
+  notifies :run, "execute[sysctl-reload]"
 end
