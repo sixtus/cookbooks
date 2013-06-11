15ae1f7d0e39b176783c8cd4416d71706b285876
Benedikt Böhm <bb@xnull.de>
2013-06-11 14:56:13 +0200

    lxc: add lxc-gentoo template

--- cookbooks/lxc/metadata.rb
+++ cookbooks/lxc/metadata.rb
@@ -1,10 +1,12 @@
+# encoding: utf-8
+
 description "Linux Containers"
 
 maintainer "Benedikt Böhm"
 maintainer_email "benedikt.boehm@madvertise.com"
 license "Apache v2.0"
 
-version "1.0.0"
+version "1.0.1"
 
 supports "debian"
 supports "gentoo"
--- cookbooks/lxc/recipes/default.rb
+++ cookbooks/lxc/recipes/default.rb
@@ -7,4 +7,11 @@ when "debian"
   package "lxc"
 end
 
+cookbook_file "/usr/share/lxc/templates/lxc-gentoo" do
+  source "lxc-gentoo"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
 systemd_unit "lxc@.service"
