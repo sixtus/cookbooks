6ee2ae32a1e7c68bb33f453ccd1af174012e55dd
Benedikt Böhm <bb@xnull.de>
2013-07-05 09:33:07 +0200

    duply: change default backup path to /var/backup

--- cookbooks/duply/definitions/backup.rb
+++ cookbooks/duply/definitions/backup.rb
@@ -1,6 +1,6 @@
 define :duply_backup,
   :source => nil,
-  :max_full_backups => 1,
+  :max_full_backups => 3,
   :max_full_age => "1M",
   :volume_size => "25M" do
   node.set[:backup][:configs][params[:name]] = params
