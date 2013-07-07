94f2fb343950becc30f9779a420f2fbe99b64b39
Benedikt Böhm <bb@xnull.de>
2013-07-07 13:15:06 +0200

    duply: fix typo in volume size specification

--- cookbooks/duply/definitions/backup.rb
+++ cookbooks/duply/definitions/backup.rb
@@ -2,6 +2,6 @@ define :duply_backup,
   :source => nil,
   :max_full_backups => 3,
   :max_full_age => "1M",
-  :volume_size => "25M" do
+  :volume_size => 25 do
   node.set[:backup][:configs][params[:name]] = params
 end
