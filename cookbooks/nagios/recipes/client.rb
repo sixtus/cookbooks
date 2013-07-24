4ec0065155b3b87600f2142c09d00b1d6b178d4a
Benedikt Böhm <bb@xnull.de>
2013-07-24 17:12:32 +0200

    nagios: fix regression in ruby wrapper

--- cookbooks/nagios/recipes/client.rb
+++ cookbooks/nagios/recipes/client.rb
@@ -14,6 +14,18 @@ when "gentoo"
     mode "0755"
   end
 
+  directory "/usr/lib/ruby/site_ruby/nagios" do
+    owner "root"
+    group "root"
+    mode "0755"
+  end
+
+  directory "/usr/lib/ruby/site_ruby/nagios/plugin" do
+    owner "root"
+    group "root"
+    mode "0755"
+  end
+
   cookbook_file "/usr/lib/ruby/site_ruby/nagios/plugin.rb" do
     source "plugin.rb"
     owner "root"
