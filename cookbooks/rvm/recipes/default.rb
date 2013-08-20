3da39a178f2623d08cb4b15709f38db62578ad5a
Benedikt Böhm <bb@xnull.de>
2013-08-20 13:43:22 +0200

    rvm: force upgrade of rvm gem

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -5,12 +5,12 @@ if root?
     pkg = package "dev-ruby/rvm" do
       action :nothing
     end
-    pkg.run_action(:install)
+    pkg.run_action(:upgrade)
     Gem.clear_paths
   else
     chef_gem 'rvm' do
       action :install
-      version '>= 1.11.3.6'
+      version '>= 1.11.3.8'
     end
   end
 
