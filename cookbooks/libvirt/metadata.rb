dc87368173afa6d799486af6578d72b775f93e5f
Benedikt Böhm <bb@xnull.de>
2013-08-24 19:54:12 +0200

    libvirt: add config file

--- cookbooks/libvirt/metadata.rb
+++ cookbooks/libvirt/metadata.rb
@@ -4,6 +4,6 @@ maintainer "Benedikt Böhm"
 maintainer_email "bb@xnull.de"
 license "Apache v2.0"
 
-version "0.1.0"
+version "0.1.1"
 
 supports "gentoo"
--- cookbooks/libvirt/recipes/default.rb
+++ cookbooks/libvirt/recipes/default.rb
@@ -1,5 +1,13 @@
 package "app-emulation/libvirt"
 
+cookbook_file "/etc/libvirt/libvirt.conf" do
+  source "libvirt.conf"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :restart, "service[libvirtd]"
+end
+
 service "libvirtd" do
   action [:enable, :start]
 end
