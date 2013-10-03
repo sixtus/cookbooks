06cbdb9e7805700d586779b6a9bcaa0e16a916d5
Benedikt Böhm <bb@xnull.de>
2013-10-03 08:28:46 +0200

    virtualbox: only root mode is supported

--- cookbooks/virtualbox/metadata.rb
+++ cookbooks/virtualbox/metadata.rb
@@ -4,6 +4,6 @@ maintainer "Benedikt Böhm"
 maintainer_email "bb@xnull.de"
 license "Apache v2.0"
 
-version "0.2.0"
+version "0.2.1"
 
 supports "gentoo"
--- cookbooks/virtualbox/recipes/default.rb
+++ cookbooks/virtualbox/recipes/default.rb
@@ -2,14 +2,16 @@ package "app-emulation/virtualbox"
 package "app-emulation/virtualbox-modules"
 package "app-emulation/vagrant"
 
-file "/etc/modules-load.d/virtualbox.conf" do
-  content "vboxnetflt\nvboxnetadp\nvboxpci\nvboxdrv\n"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :restart, 'service[systemd-modules-load.service]'
-end
+if root?
+  file "/etc/modules-load.d/virtualbox.conf" do
+    content "vboxnetflt\nvboxnetadp\nvboxpci\nvboxdrv\n"
+    owner "root"
+    group "root"
+    mode "0644"
+    notifies :restart, 'service[systemd-modules-load.service]'
+  end
 
-shorewall_interface "net" do
-  interface "vboxnet0 0.0.0.0 optional"
+  shorewall_interface "net" do
+    interface "vboxnet0 0.0.0.0 optional"
+  end
 end
