4996cb869e51cfd35114fae5cd233b9c883dfc97
Benedikt Böhm <bb@xnull.de>
2013-10-08 21:39:04 +0200

    lxc: fix debian support

--- cookbooks/lxc/recipes/default.rb
+++ cookbooks/lxc/recipes/default.rb
@@ -8,6 +8,18 @@ when "debian"
 
 end
 
+directory "/usr/share/lxc" do
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+directory "/usr/share/lxc/templates" do
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
 cookbook_file "/usr/share/lxc/templates/lxc-gentoo" do
   source "lxc-gentoo"
   owner "root"
