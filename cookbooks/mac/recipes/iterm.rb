d31bf511a0a142141b5dd880939b0ebdf7154158
Benedikt Böhm <bb@xnull.de>
2013-10-13 23:22:45 +0200

    mac: install menlo font

--- cookbooks/mac/recipes/iterm.rb
+++ cookbooks/mac/recipes/iterm.rb
@@ -4,6 +4,16 @@ if platform?("mac_os_x")
     zip true
   end
 
+  execute "fc-cache-menlo-powerline" do
+    command "/opt/X11/bin/fc-cache"
+    action :nothing
+  end
+
+  remote_file "#{node[:homedir]}/Library/Fonts/Menlo-Powerline.otf" do
+    source "https://gist.github.com/raw/1595572/417a3fa36e35ca91d6d23ac961071094c26e5fad/Menlo-Powerline.otf"
+    notifies :run, "execute[fc-cache-menlo-powerline]"
+  end
+
   mac_userdefaults "hide tab bar when there is only one tab" do
     domain "com.googlecode.iterm2"
     key "HideTab"
