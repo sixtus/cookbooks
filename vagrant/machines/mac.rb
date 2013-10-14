db5123ae8b79f8ef49b797486777a33f867bea98
Benedikt Böhm <bb@xnull.de>
2013-10-14 14:39:30 +0200

    add hostnames to vagrant machines

--- vagrant/machines/mac.rb
+++ vagrant/machines/mac.rb
@@ -1,8 +1,9 @@
 Vagrant.configure('2') do |config|
 
   # mac os x
-  config.vm.define 'mac_os_x' do |base|
+  config.vm.define 'mac' do |base|
     base.vm.box = 'mac_os_x-10.8.5+chef'
+    base.vm.hostname = "mac.zenops.ws"
     base.vm.synced_folder '.', '/vagrant', disabled: true
     setup_network(base, 5402)
   end
--- vagrant/machines/zentoo.rb
+++ vagrant/machines/zentoo.rb
@@ -3,6 +3,7 @@ Vagrant.configure('2') do |config|
   config.vm.define 'zentoo' do |base|
     base.vm.box = 'zentoo-amd64-base'
     base.vm.box_url = 'http://mirror.zenops.net/zentoo/amd64/zentoo-amd64-base.box'
+    base.vm.hostname = "zentoo.zenops.ws"
     setup_network(base, 5002)
     setup_chef_solo(base) do |chef|
       chef.add_role("base")
