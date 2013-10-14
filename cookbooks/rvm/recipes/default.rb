dfd05bdc9db0120f99aa60c8a76129beee57c39d
Benedikt Böhm <bb@xnull.de>
2013-10-14 22:37:54 +0200

    rvm: add debian support

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -1,16 +1,23 @@
-# install rvm api gem during chef compile phase
+# this looks a little bit silly, but we need to ensure that the rvm gem is
+# installed properly, even for non-root chef-solo users which are not using
+# rvm/bundler (yet).
 if gentoo?
-  pkg = package "dev-ruby/rvm" do
+  # portage package provider will take care of using sudo
+  package "dev-ruby/rvm" do
     action :nothing
-  end
-  pkg.run_action(:upgrade)
-  Gem.clear_paths
+  end.run_action(:upgrade)
+elsif debian_based?
+  execute "sudo -H gem install rvm" do
+    action :nothing
+  end.run_action(:run)
 else
   chef_gem 'rvm' do
     action :install
     version '>= 1.11.3.8'
   end
 end
+
+Gem.clear_paths
 
 require 'rvm'
 
