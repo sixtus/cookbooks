80b23fdfa9ddfb102a1a879db4253f6bca41b65b
Benedikt Böhm <bb@xnull.de>
2013-11-03 14:13:28 +0100

    bash/python: add virtualenvwrapper support

--- cookbooks/python/recipes/virtualenv.rb
+++ cookbooks/python/recipes/virtualenv.rb
@@ -2,10 +2,13 @@ include_recipe "python::pip"
 
 if gentoo?
   package "dev-python/virtualenv"
+  package "dev-python/virtualenvwrapper"
 elsif debian_based?
   package "python-virtualenv"
+  python_pip "virtualenvwrapper"
 elsif mac_os_x?
   python_pip "virtualenv"
+  python_pip "virtualenvwrapper"
 else
   raise "platform not supported"
 end
