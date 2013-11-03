62a71039ceedec04995b7f5619a1e9bfd10a84f7
Benedikt Böhm <bb@xnull.de>
2013-11-03 15:05:50 +0100

    rvm: update rvm version

--- cookbooks/rvm/metadata.rb
+++ cookbooks/rvm/metadata.rb
@@ -6,7 +6,7 @@ maintainer "Benedikt Böhm"
 maintainer_email "bb@xnull.de"
 license "Apache v2.0"
 
-version "2.3.1"
+version "2.3.2"
 
 supports "debian"
 supports "gentoo"
--- cookbooks/rvm/resources/instance.rb
+++ cookbooks/rvm/resources/instance.rb
@@ -1,6 +1,6 @@
 actions :create, :delete
 
-attribute :version, :kind_of => String, :default => "1.23.5"
+attribute :version, :kind_of => String, :default => "1.23.12"
 
 def initialize(name, run_context=nil)
   super
