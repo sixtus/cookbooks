23213b1453b2f77778820b3eff6eea374968f87e
Benedikt Böhm <bb@xnull.de>
2013-11-04 16:59:13 +0100

    mac: ignore missing storerc

--- cookbooks/mac/attributes/default.rb
+++ cookbooks/mac/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:apple_id] = File.read("#{homedir}/.storerc").split.first
-default[:apple_password] = File.read("#{homedir}/.storerc").split.last
+default[:apple_id] = File.read("#{homedir}/.storerc").split.first rescue nil
+default[:apple_password] = File.read("#{homedir}/.storerc").split.last rescue nil
 default[:mac][:apps] = {}
