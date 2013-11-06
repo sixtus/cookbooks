515caa25fb508dd0b4fa338837e7cd7104984979
Igor Susman <igor.susman@liquidm.com>
2013-11-06 14:50:12 +0100

    vim: revert powerline cahce enabled globally

--- cookbooks/vim/metadata.rb
+++ cookbooks/vim/metadata.rb
@@ -1,12 +1,12 @@
-# encoding: utf-8
 
+# encoding: utf-8
 description "Vim is a highly configurable text editor"
 
 maintainer "Benedikt Böhm"
 maintainer_email "bb@xnull.de"
 license "Apache v2.0"
 
-version "2.1.1"
+version "2.1.3"
 
 supports "debian"
 supports "gentoo"
