225b8435e8d5e42b7707bff8aa87db5e63cbf5d6
Benedikt Böhm <bb@xnull.de>
2013-11-14 20:48:00 +0100

    quickstart: remove all partitions/volume groups/arrays

--- tasks/templates/quickstart.sh
+++ tasks/templates/quickstart.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+umount -l /mnt/* 2>&1 > /dev/null
+
 vgchange -a n
+echo -ne "vgremove vg\ny\ny\ny\ny\ny\ny\ny\ny\ny\nquit\n" | lvm 2>&1 > /dev/null
+vgreduce --removemissing vg
 
 for i in /dev/md*; do
-	mdadm -S $i
+    mdadm -S $i 2>&1 > /dev/null
 done
+
+for x in /dev/sd*; do
+    mdadm --zero-superblock $x 2>&1 > /dev/null
+done
+
+for x in /dev/sd[a-z]; do
+    echo -ne "rm 1\nIgnore\nrm 2\nIgnore\nrm 128\nIgnore\nquit\n" | parted $x 2>&1 > /dev/null
+done
+
+partprobe
 
 cd /tmp
 wget -q -O quickstart.tar.gz https://github.com/zentoo/quickstart/tarball/master
