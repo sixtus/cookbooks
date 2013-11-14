9286c571f043db4070a09ae347ada4e6593af5dc
Benedikt Böhm <bb@xnull.de>
2013-11-14 08:53:24 +0100

    mac: fix minor bootstrap issues

--- cookbooks/base/metadata.rb
+++ cookbooks/base/metadata.rb
@@ -6,7 +6,7 @@ maintainer "Benedikt Böhm"
 maintainer_email "bb@xnull.de"
 license "Apache v2.0"
 
-version "6.8.2"
+version "6.8.3"
 
 supports "debian"
 supports "gentoo"
--- cookbooks/mac/recipes/drive.rb
+++ cookbooks/mac/recipes/drive.rb
@@ -1,5 +1,6 @@
 if mac_os_x?
   mac_package "Google Drive" do
     source "http://r13---sn-h0j7sn7s.c.pack.google.com/edgedl/drive/installgoogledrive.dmg"
+    volumes_dir "Install Google Drive"
   end
 end
