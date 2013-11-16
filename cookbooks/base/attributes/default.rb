cbfdaf9f8347622bc09fa90f504aaa8622b51608
Benedikt Böhm <bb@xnull.de>
2013-11-16 20:46:17 +0100

    base: use hetzner DNS if we are inside hetzners network

--- cookbooks/base/attributes/default.rb
+++ cookbooks/base/attributes/default.rb
@@ -49,8 +49,13 @@ default[:locales] = [
 ]
 
 # nameservers and search domain
+%x(whois #{node[:primary_ipaddress]} | grep -q HETZNER-RZ)
+if $?.exitstatus == 0
+  default[:resolv][:nameservers] = %w(213.133.98.98 213.133.99.99)
+else
+  default[:resolv][:nameservers] = %w(4.2.2.2 8.8.8.8)
+end
 default[:resolv][:search] = []
-default[:resolv][:nameservers] = %w(4.2.2.2 8.8.8.8)
 default[:resolv][:hosts] = []
 default[:resolv][:aliases] = []
 
