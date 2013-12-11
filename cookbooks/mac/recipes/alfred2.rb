d68c0629443be938ec369a2539bbf83c065382b5
Benedikt Böhm <bb@xnull.de>
2013-12-11 23:23:16 +0100

    mac: fix Alfred2 URL

--- cookbooks/mac/recipes/alfred2.rb
+++ cookbooks/mac/recipes/alfred2.rb
@@ -1,6 +1,6 @@
 if mac_os_x?
   mac_package "Alfred 2" do
-    source "http://cachefly.alfredapp.com/Alfred_2.0.3_187.zip"
+    source "http://cachefly.alfredapp.com/Alfred_2.1.1_227.zip"
     type "zip_app"
   end
 end
