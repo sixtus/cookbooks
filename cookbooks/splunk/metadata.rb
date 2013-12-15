8239b6ba577a0d92598200f753f4576e9cb8c0f4
Benedikt Böhm <bb@xnull.de>
2013-12-15 19:03:04 +0100

    splunk: fix foodcritic warnings

--- cookbooks/splunk/metadata.rb
+++ cookbooks/splunk/metadata.rb
@@ -6,7 +6,7 @@ maintainer "Benedikt Böhm"
 maintainer_email "bb@xnull.de"
 license "Apache v2.0"
 
-version "2.7.0"
+version "2.7.1"
 
 supports "debian"
 supports "gentoo"
--- cookbooks/splunk/recipes/forwarder.rb
+++ cookbooks/splunk/recipes/forwarder.rb
@@ -3,14 +3,14 @@ if splunk_forwarder?
     package "net-analyzer/splunkforwarder"
 
   elsif debian_based?
-    remote_file "/tmp/splunkforwarder-5.0.2-149561-linux-2.6-amd64.deb" do
+    remote_file "#{Chef::Config[:file_cache_path]}/splunkforwarder-5.0.2-149561-linux-2.6-amd64.deb" do
       source "http://www.splunk.com/page/download_track?file=5.0.2/universalforwarder/linux/splunkforwarder-5.0.2-149561-linux-2.6-amd64.deb&ac=get_splunk_download&wget=true&name=wget&typed=releases"
       mode "0644"
       checksum "e4a16af9764880c2784abad6b2b77e86c65b684a0c24dfbbcf60078e3f152feb"
     end
 
     dpkg_package "splunkforwarder" do
-      source "/tmp/splunkforwarder-5.0.2-149561-linux-2.6-amd64.deb"
+      source "#{Chef::Config[:file_cache_path]}/splunkforwarder-5.0.2-149561-linux-2.6-amd64.deb"
       action :install
     end
 
