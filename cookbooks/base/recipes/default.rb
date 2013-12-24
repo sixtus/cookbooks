b7db0d14674b9f0883358a15cc9c165fa459f1f1
Benedikt Boehm <bene@bene.dev.zenops.net>
2013-12-24 22:47:18 +0100

    base: move some cookbooks to role[base]

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -9,13 +9,3 @@ end
 # load platform recipes
 include_recipe "base::linux" if linux?
 include_recipe "base::mac_os_x" if mac_os_x?
-
-# load common recipes
-include_recipe "bash"
-include_recipe "git"
-include_recipe "htop"
-include_recipe "lftp"
-include_recipe "python"
-include_recipe "ssh"
-include_recipe "tmux"
-include_recipe "vim"
--- roles/base.rb
+++ roles/base.rb
@@ -2,4 +2,12 @@ description "base role for all nodes"
 
 run_list(%w(
   recipe[base]
+  recipe[bash]
+  recipe[git]
+  recipe[htop]
+  recipe[lftp]
+  recipe[python]
+  recipe[ssh]
+  recipe[tmux]
+  recipe[vim]
 ))
