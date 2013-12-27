7214eef2e5ddb9b2b74aeb1574893107223acbc0
Benedikt Böhm <bb@xnull.de>
2013-12-27 21:06:22 +0100

    rvm: fix non-root install

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -1,29 +1,13 @@
-# this looks a little bit silly, but we need to ensure that the rvm gem is
-# installed properly, even for non-root chef-solo users which are not using
-# rvm/bundler (yet).
 if gentoo?
   # portage package provider will take care of using sudo
   package "dev-ruby/rvm" do
     action :nothing
   end.run_action(:upgrade)
-elsif debian_based?
-  chef_gem "rvm" do
-    action :nothing
-  end.run_action(:upgrade)
-elsif mac_os_x?
-  # need to install into omnibus if available. otherwise mac users will use
-  # bundled gems.
-  execute "sudo -H /opt/chef/embedded/bin/gem install rvm -v 1.11.3.8" do
-    action :nothing
-    only_if do
-      File.exist?("/opt/chef/embedded/bin/gem") and
-      !File.exist?("/opt/chef/embedded/lib/ruby/gems/1.9.1/gems/rvm-1.11.3.8")
-    end
-  end.run_action(:run)
 else
+  # omnibus ruby
   chef_gem 'rvm' do
     action :install
-    version '>= 1.11.3.8'
+    options "--user-install" if !root?
   end
 end
 
