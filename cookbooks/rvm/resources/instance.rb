37d7eaa131f6b7776d65e3805f1d4ae3d37a77b2
Benedikt Böhm <bb@xnull.de>
2014-01-04 12:42:10 +0100

    fix chef bootstrap

--- cookbooks/rvm/resources/instance.rb
+++ cookbooks/rvm/resources/instance.rb
@@ -1,6 +1,6 @@
 actions :create, :delete
 
-attribute :version, :kind_of => String, :default => "1.24.8"
+attribute :version, :kind_of => String, :default => "1.25.14"
 
 def initialize(name, run_context=nil)
   super
