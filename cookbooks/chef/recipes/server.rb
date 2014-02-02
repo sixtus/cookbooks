f20b585fedcfd850a7d10c1b0bccc2e1102cc638
Benedikt Böhm <bb@xnull.de>
2014-02-02 13:18:49 +0100

    chef: always run reconfigure for chef-server

--- cookbooks/chef/recipes/server.rb
+++ cookbooks/chef/recipes/server.rb
@@ -2,14 +2,13 @@ remote_file "#{Chef::Config[:file_cache_path]}/chef-server_11.0.8-1.ubuntu.12.04
   source "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.8-1.ubuntu.12.04_amd64.deb"
 end
 
-execute "chef-server-ctl-reconfigure" do
-  command "chef-server-ctl reconfigure"
-  action :nothing
-end
-
 dpkg_package "chef-server" do
   source "#{Chef::Config[:file_cache_path]}/chef-server_11.0.8-1.ubuntu.12.04_amd64.deb"
   notifies :run, "execute[chef-server-ctl-reconfigure]"
+end
+
+execute "chef-server-ctl-reconfigure" do
+  command "chef-server-ctl reconfigure"
 end
 
 shorewall_rule "chef-server" do
