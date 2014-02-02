afa448fd45ff809444fc61ac80cf6d4558b2520f
Benedikt Böhm <bb@xnull.de>
2014-02-02 13:19:16 +0100

    ganymed: disable ganymed collector in favor of upcoming collectd

--- cookbooks/ganymed/recipes/default.rb
+++ cookbooks/ganymed/recipes/default.rb
@@ -1,32 +1,3 @@
-if ganymed?
-  package "net-analyzer/ganymed" do
-    action :upgrade
-    notifies :restart, "service[ganymed]"
-  end
-
-  %w(
-    /etc/ganymed
-    /usr/lib/ganymed
-    /usr/lib/ganymed/collectors
-  ).each do |dir|
-    directory dir do
-      owner "root"
-      group "root"
-      mode "0755"
-    end
-  end
-
-  template "/etc/ganymed/config.yml" do
-    source "config.yml"
-    owner "root"
-    group "root"
-    mode "0644"
-    notifies :restart, "service[ganymed]"
-  end
-
-  systemd_unit "ganymed.service"
-
-  service "ganymed" do
-    action [:enable, :start]
-  end
+service "ganymed" do
+  action [:disable, :stop]
 end
