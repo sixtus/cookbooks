a9af7b73fde5398d23d6621b4b5c67c4ebc737bd
Benedikt Böhm <bb@xnull.de>
2014-02-17 20:39:28 +0100

    ntp: use hetzner pool if inside hetzners network

--- cookbooks/ntp/attributes/default.rb
+++ cookbooks/ntp/attributes/default.rb
@@ -1,6 +1,15 @@
-default[:ntp][:server] = %w(
-  0.de.pool.ntp.org
-  1.de.pool.ntp.org
-  2.de.pool.ntp.org
-  3.de.pool.ntp.org
-)
+%x(whois #{node[:primary_ipaddress]} | grep -q HETZNER-RZ)
+if $?.exitstatus == 0
+  default[:ntp][:server] = %w(
+    ntp1.hetzner.de
+    ntp2.hetzner.com
+    ntp3.hetzner.net
+  )
+else
+  default[:ntp][:server] = %w(
+    0.de.pool.ntp.org
+    1.de.pool.ntp.org
+    2.de.pool.ntp.org
+    3.de.pool.ntp.org
+  )
+end
