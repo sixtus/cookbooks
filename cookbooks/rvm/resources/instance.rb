2e2db589f1313ef3d097873978701151ca425cbe
Benedikt Böhm <bb@xnull.de>
2014-02-17 20:36:42 +0100

    add jzerohttp recipe

--- cookbooks/rvm/resources/instance.rb
+++ cookbooks/rvm/resources/instance.rb
@@ -1,6 +1,6 @@
 actions :create, :delete
 
-attribute :version, :kind_of => String, :default => "1.25.14"
+attribute :version, :kind_of => String, :default => "1.25.18"
 
 def initialize(name, run_context=nil)
   super
