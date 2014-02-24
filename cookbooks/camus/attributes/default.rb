09c41cff0942596e7172aecf7e0a63772c075c6b
Hagen Rother <hagen.rother@liquidm.com>
2014-02-24 14:08:36 +0100

    camus: make hdfs paths configurable

--- cookbooks/camus/attributes/default.rb
+++ cookbooks/camus/attributes/default.rb
@@ -2,6 +2,9 @@ default[:camus][:git][:repository] = "https://github.com/linkedin/camus"
 default[:camus][:git][:revision] = "camus-kafka-0.8"
 
 default[:camus][:topics] = nil
-
 default[:camus][:decoder] = nil
 default[:camus][:writer] = nil
+
+default[:camus][:etl][:destination] = "/history"
+default[:camus][:etl][:base_path] = "/camus/running"
+default[:camus][:etl][:history_path] = "/camus/state"
