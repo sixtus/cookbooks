b3b6d166021b482e7e54a06a8a1616d4c1a58ea4
Benedikt Böhm <bb@xnull.de>
2014-03-02 08:36:13 +0100

    ohai: fix attribute merging

--- cookbooks/ohai/attributes/default.rb
+++ cookbooks/ohai/attributes/default.rb
@@ -1,2 +1,7 @@
-default[:ohai][:plugin_path] = "/etc/chef/ohai/plugins"
+if root?
+  default[:ohai][:plugin_path] = "/var/lib/ohai/plugins"
+else
+  default[:ohai][:plugin_path] = "#{get_user(node[:current_user])[:dir]}/.ohai/plugins"
+end
+
 default[:ohai][:plugins][:ohai] = "plugins"
--- cookbooks/ohai/recipes/default.rb
+++ cookbooks/ohai/recipes/default.rb
@@ -1,26 +1,18 @@
-reload_ohai = false
-
-unless Ohai::Config[:plugin_path].include?(node[:ohai][:plugin_path])
-  # we deliberately skip the plugins shipped with ohai. we have them all in our
-  # cookbook anyway
-  Ohai::Config[:plugin_path] = [node[:ohai][:plugin_path]].flatten.compact
-  reload_ohai ||= true
-end
-
-node[:ohai][:plugins].each_pair do |source_cookbook, path|
-  rd = remote_directory node[:ohai][:plugin_path] do
-    cookbook source_cookbook
-    source path
-    mode '0755'
-    recursive true
-    purge false
-    action :nothing
-  end
-  rd.run_action(:create)
-  reload_ohai ||= rd.updated?
-end
-
-resource = ohai 'custom_plugins' do
+remote_directory node[:ohai][:plugin_path] do
+  source "plugins"
+  mode "0755"
+  recursive true
+  purge false
   action :nothing
-end
-resource.run_action(:reload) if reload_ohai
+end.run_action(:create)
+
+Ohai::Config[:plugin_path] = [node[:ohai][:plugin_path]]
+
+ohai = ::Ohai::System.new
+ohai.all_plugins
+
+recipes, roles = node.automatic_attrs[:recipes], node.automatic_attrs[:roles]
+node.automatic_attrs.clear
+node.automatic_attrs.merge!(ohai.data)
+node.automatic_attrs[:recipes] = recipes
+node.automatic_attrs[:roles] = roles
