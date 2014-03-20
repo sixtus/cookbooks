33c5f083096ebdd1267d9b18507237b331215b06
Hagen Rother <hagen.rother@liquidm.com>
2014-03-20 13:41:03 +0100

    druid: dumbo: fix one more path

--- cookbooks/druid/templates/default/dumbo-runner.sh
+++ cookbooks/druid/templates/default/dumbo-runner.sh
@@ -18,7 +18,7 @@ while true; do
   ) 9>/var/app/dumbo/run.lock
 
   # spawn hadoop
-  for conf in /var/app/dumbo/current/*.druid; do
+  for conf in /var/app/dumbo/*.druid; do
     /var/app/dumbo/bin/batch-druid-job.sh $conf &
     sleep 10
   done
