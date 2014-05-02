a82aed96dda4da61f3aa097ae311a51e49b35e4d
Benedikt Böhm <bb@xnull.de>
2014-05-02 11:01:19 +0200

    chef: client should verify SSL peers

--- cookbooks/chef/templates/default/client.rb
+++ cookbooks/chef/templates/default/client.rb
@@ -6,7 +6,7 @@ log_location STDOUT
 verbose_logging false
 enable_reporting false
 
-ssl_verify_mode :verify_none
+ssl_verify_mode :verify_peer
 chef_server_url "<%= node[:chef][:client][:server_url] %>"
 
 file_cache_path "/var/lib/chef/cache"
