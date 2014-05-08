76b144f2d2931ac40a8c5ddcc2060c17411a497d
Hagen Rother <hagen.rother@liquidm.com>
2014-05-08 17:55:55 +0200

    druid: remove dumbo fallbacks, there none anymore

--- cookbooks/druid/templates/default/batch-druid-job.sh
+++ cookbooks/druid/templates/default/batch-druid-job.sh
@@ -12,11 +12,6 @@ job_file=$1
 
   echo BATCH_IMPORT $job_file
   java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -classpath $CLASSPATH io.druid.cli.Main index hadoop $job_file
-  if [ $? -ne 0 ]; then
-      echo BATCH_FALLBACK $job_file
-      java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 -classpath $CLASSPATH io.druid.cli.Main index hadoop $job_file.fallback
-  fi
   sleep 600 # wait for cluster to distribute
   rm $job_file # might have been regenerated
-  rm $job_file.fallback
 ) 42<$job_file
