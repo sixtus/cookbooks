51a684eb558fc1ee82ed1178c90c1a6fff8b6495
Hagen Rother <hagen.rother@liquidm.com>
2014-05-16 00:28:59 +0200

    add debug output to quickstart
    Conflicts:
    	ca/index
    	ca/index.old
    	ca/serial
    	ca/serial.old

--- tasks/templates/quickstart.sh
+++ tasks/templates/quickstart.sh
@@ -24,4 +24,4 @@ cd /tmp
 wget -q -O quickstart.tar.gz https://github.com/zentoo/quickstart/tarball/master
 tar -xzf quickstart.tar.gz
 cd *-quickstart-*
-exec ./quickstart profiles/<%= args.profile %>.sh
+exec ./quickstart -v -d profiles/<%= args.profile %>.sh
