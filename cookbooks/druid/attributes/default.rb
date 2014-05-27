7e3ba1e21d04ba23840132ef8597a5884f12f553
Hagen Rother <hagen.rother@liquidm.com>
2014-05-27 15:46:37 +0200

    druid: make indexing actually cluster again

--- cookbooks/druid/attributes/default.rb
+++ cookbooks/druid/attributes/default.rb
@@ -54,7 +54,7 @@ default[:druid][:indexer][:dm]                  = "64m"
 default[:druid][:indexer][:runner][:javaOpts]   = "-d64 -server -Xmx8g"
 default[:druid][:indexer][:runner][:startPort]  = 8092
 default[:druid][:indexer][:workers]             = [node[:cpu][:total]-1,1].max
-default[:druid][:indexing][:service]            = node[:fqdn]
+default[:druid][:indexing][:service]            = node.cluster_name
 
 default[:druid][:server][:max_size] = 0
 default[:druid][:server][:tier] = "default"
