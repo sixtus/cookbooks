a8bd064dc1a2b888df24c0837b27f6a19f437b8a
Benedikt Böhm <bb@xnull.de>
2014-06-08 23:47:59 +0200

    newrelic: only support gentoo

--- cookbooks/newrelic/recipes/default.rb
+++ cookbooks/newrelic/recipes/default.rb
@@ -1,12 +1,14 @@
-package "sys-apps/newrelic-sysmond"
+if gentoo?
+  package "sys-apps/newrelic-sysmond"
 
-execute "newrelic-license-key" do
-  command "/usr/sbin/nrsysmond-config --set license_key=#{node[:newrelic][:license_key]}"
-  only_if { !!node[:newrelic][:license_key] }
-end
+  execute "newrelic-license-key" do
+    command "/usr/sbin/nrsysmond-config --set license_key=#{node[:newrelic][:license_key]}"
+    only_if { !!node[:newrelic][:license_key] }
+  end
 
-systemd_unit "newrelic-sysmond.service"
+  systemd_unit "newrelic-sysmond.service"
 
-service "newrelic-sysmond" do
-  action [:enable, :start]
+  service "newrelic-sysmond" do
+    action [:enable, :start]
+  end
 end
