1323ceac0cfd1bc4cfe41ab95225c95d00654577
Benedikt Böhm <bb@xnull.de>
2014-06-09 01:28:32 +0200

    newrelic: make recipe idempotent

--- cookbooks/newrelic/recipes/default.rb
+++ cookbooks/newrelic/recipes/default.rb
@@ -4,6 +4,7 @@ if gentoo?
   execute "newrelic-license-key" do
     command "/usr/sbin/nrsysmond-config --set license_key=#{node[:newrelic][:license_key]}"
     only_if { !!node[:newrelic][:license_key] }
+    not_if { File.read("/etc/newrelic/nrsysmond.cfg").match(/license_key=#{node[:newrelic][:license_key]}/) }
   end
 
   systemd_unit "newrelic-sysmond.service"
