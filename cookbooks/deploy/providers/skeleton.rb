4616c70edf13a5b8be508a06120f4648a07ef673
Benedikt Böhm <bb@xnull.de>
2014-06-11 12:09:13 +0200

    deploy: add support for hard coded uid/gid

--- cookbooks/deploy/providers/skeleton.rb
+++ cookbooks/deploy/providers/skeleton.rb
@@ -3,27 +3,31 @@ include AccountHelpers
 use_inline_resources
 
 action :create do
-  user = new_resource.name
-  groups = new_resource.groups
-  key_source = new_resource.key_source
-  akf = new_resource.authorized_keys_for
+  nr = new_resource
+  user = nr.name
+  groups = nr.groups
+  key_source = nr.key_source
+  akf = nr.authorized_keys_for
 
   if akf != false
     akf = node[:deploy][:deployers] if akf.nil? or akf.empty?
   end
 
-  if new_resource.homedir == nil
+  if nr.homedir == nil
     homedir = get_user(user)[:dir] || "/var/app/#{user}"
   else
-    homedir = new_resource.homedir
+    homedir = nr.homedir
   end
 
-  group user
+  group user do
+    gid nr.gid if nr.gid
+  end
 
   account_skeleton user do
     comment user
     home homedir
     home_mode "0755"
+    uid nr.uid if nr.uid
     gid user
     groups groups
     authorized_keys_for akf if akf
@@ -42,7 +46,7 @@ action :create do
     end
   end
 
-  shared = %w(cache config log pids system) + new_resource.shared
+  shared = %w(cache config log pids system) + nr.shared
 
   shared.uniq.each do |d|
     directory "#{homedir}/shared/#{d}" do
--- cookbooks/deploy/resources/skeleton.rb
+++ cookbooks/deploy/resources/skeleton.rb
@@ -5,6 +5,8 @@ attribute :groups, :kind_of => Array, :default => []
 attribute :shared, :kind_of => Array, :default => []
 attribute :key_source, :kind_of => String
 attribute :authorized_keys_for, :kind_of => [Symbol, Array, FalseClass]
+attribute :uid, kind_of: Integer
+attribute :gid, kind_of: Integer
 
 def initialize(name, run_context=nil)
   super(name, run_context)
