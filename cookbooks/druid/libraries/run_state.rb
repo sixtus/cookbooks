cab89ce2cfd94f3bed67125569e2cfb399759de8
Benedikt Böhm <bb@xnull.de>
2014-06-26 11:48:22 +0200

    druid: use mvn helper

--- cookbooks/druid/libraries/run_state.rb
+++ cookbooks/druid/libraries/run_state.rb
@@ -1,6 +1,6 @@
 module DruidHelpers
   def druid_version
-    @druid_version ||= %x{cd /var/app/druid/current && /usr/bin/mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\\[' | grep -v Download}.strip
+    @druid_version ||= mvn_project_version("/var/app/druid/current")
   end
 end
 
