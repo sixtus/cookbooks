ac04710d8a3e3f195052e05687c852ab80d53ac2
Benedikt Böhm <bb@xnull.de>
2014-06-27 10:53:26 +0200

    mac: update to Monkey Office 2014

--- cookbooks/mac/recipes/monkeyoffice.rb
+++ cookbooks/mac/recipes/monkeyoffice.rb
@@ -1,5 +1,5 @@
 if mac_os_x?
-  mac_package "Monkey Office 2013" do
-    source "http://monkey-office.de/download/current/mac/MonKey_Office_2013.dmg"
+  mac_package "Monkey Office 2014" do
+    source "http://monkey-office.de/download/current/mac/MonKey_Office_2014.dmg"
   end
 end
