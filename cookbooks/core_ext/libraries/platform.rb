d9a99836af22cb2291fa13320713f49c4df58310
Benedikt Böhm <bb@xnull.de>
2014-07-06 00:43:50 +0200

    core_ext: use virtualization attributes from ohai

--- cookbooks/core_ext/libraries/platform.rb
+++ cookbooks/core_ext/libraries/platform.rb
@@ -44,11 +44,11 @@ module PlatformHelpers
   end
 
   def vbox?
-    root? && File.exist?("/proc/modules") && File.read("/proc/modules") =~ /^vboxguest/
+    node[:virtualization][:system] == "vbox" && node[:virtualization][:role] == "guest"
   end
 
   def lxc?
-    root? && File.read("/proc/1/environ").split("\0").any? { |env| env =~ /lxc/ }
+    node[:virtualization][:system] == "lxc" && node[:virtualization][:role] == "guest"
   end
 end
 
