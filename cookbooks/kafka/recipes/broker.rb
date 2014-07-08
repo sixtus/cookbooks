9741cd834fde21b6f4b0b07cfb029f1767b68790
Benedikt Böhm <bb@xnull.de>
2014-07-08 21:05:11 +0200

    kafka: create zookeeper chroot

--- cookbooks/kafka/recipes/broker.rb
+++ cookbooks/kafka/recipes/broker.rb
@@ -16,6 +16,17 @@ template "/var/app/kafka/current/config/server.properties" do
   group "kafka"
   mode "0640"
   notifies :restart, "service[kafka]"
+  notifies :create, "ruby_block[kafka-zk-chroot]"
+end
+
+include_recipe "zookeeper::ruby"
+
+ruby_block "kafka-zk-chroot" do
+  action :nothing
+  block do
+    require 'zk'
+    ZK.new(zookeeper_connect(node[:kafka][:zookeeper][:root], node[:kafka][:zookeeper][:cluster]))
+  end
 end
 
 systemd_unit "kafka.service"
