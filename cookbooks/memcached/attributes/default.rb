80084d094aa162a2267d99317b4e42bb7da402b4
Benedikt Böhm <bb@xnull.de>
2014-07-26 17:27:02 +0200

    memcached: fix systemd unit

--- cookbooks/memcached/attributes/default.rb
+++ cookbooks/memcached/attributes/default.rb
@@ -1,5 +1,4 @@
 default[:memcached][:memory] = "1024"
-default[:memcached][:user] = "memcached"
 default[:memcached][:maxconn] = "1024"
 default[:memcached][:listen] = "0.0.0.0"
 default[:memcached][:port] = "11211"
