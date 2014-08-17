7cec6285f9fd7f5fd7f6443335607c057f6119ea
Benedikt Böhm <bb@xnull.de>
2014-08-17 19:24:58 +0200

    camus: update to latest master

--- cookbooks/camus/attributes/default.rb
+++ cookbooks/camus/attributes/default.rb
@@ -1,13 +1,16 @@
 default[:camus][:git][:repository] = "https://github.com/linkedin/camus"
-default[:camus][:git][:revision] = "camus-kafka-0.8"
+default[:camus][:git][:revision] = "master"
 
 default[:camus][:cluster] = node.cluster_name
 default[:camus][:topics] = nil
-default[:camus][:decoder] = nil
-default[:camus][:writer] = nil
 
-default[:camus][:etl][:destination] = "/history"
-default[:camus][:etl][:base_path] = "/camus/running"
-default[:camus][:etl][:history_path] = "/camus/state"
+default[:camus][:etl][:destination] = "/history/#{node.cluster_name}"
+default[:camus][:etl][:base_path] = "/camus/#{node.cluster_name}/running"
+default[:camus][:etl][:history_path] = "/camus/#{node.cluster_name}/state"
+default[:camus][:etl][:writer] = "com.linkedin.camus.etl.kafka.common.JSONRecordWriterProvider"
+
+default[:camus][:message][:decoder] = "com.linkedin.camus.etl.kafka.coders.JsonStringMessageDecoder"
+default[:camus][:message][:timestamp][:field] = "ts"
+default[:camus][:message][:timestamp][:format] = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 
 default[:camus][:hadoop][:path] = "/var/app/hadoop2/current/bin"
