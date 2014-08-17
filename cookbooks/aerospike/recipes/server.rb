2025c7fd855b1dc694d4001341e7bd61ceef2989
Benedikt Böhm <bb@xnull.de>
2014-08-17 11:02:24 +0200

    aerospike: add amc config

--- cookbooks/aerospike/recipes/server.rb
+++ cookbooks/aerospike/recipes/server.rb
@@ -16,6 +16,14 @@ service "aerospike" do
   action [:enable, :start]
 end
 
+template "/opt/amc/config/gunicorn_config.py" do
+  source "gunicorn.py"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :restart, "service[amc]"
+end
+
 service "amc" do
   action [:enable, :start]
 end
