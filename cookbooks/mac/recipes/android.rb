aa9cf58e92496605a80ff94e37f6e80bf9066b94
Benedikt Böhm <bb@xnull.de>
2014-08-17 09:58:48 +0200

    mac: update android sdk

--- cookbooks/mac/recipes/android.rb
+++ cookbooks/mac/recipes/android.rb
@@ -3,12 +3,12 @@ if mac_os_x?
     source "https://dl.google.com/dl/androidjumper/mtp/current/androidfiletransfer.dmg"
   end
 
-  tar_extract "http://dl.google.com/android/adt/adt-bundle-mac-x86_64-20131030.zip" do
+  tar_extract "http://dl.google.com/android/adt/adt-bundle-mac-x86_64-20140702.zip" do
     target_dir "/Applications"
     not_if { File.exist?("/Applications/Android SDK") }
   end
 
-  execute "mv /Applications/adt-bundle-mac-x86_64-20131030 '/Applications/Android SDK'" do
+  execute "mv /Applications/adt-bundle-mac-x86_64-20140702 '/Applications/Android SDK'" do
     not_if { File.exist?("/Applications/Android SDK") }
   end
 end
