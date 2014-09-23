cc4f177ec1598bcb65ad641007604ea548c1abe1
Benedikt Böhm <bb@xnull.de>
2014-09-24 00:10:40 +0200

    camus: adapt to latest changes

--- cookbooks/camus/attributes/default.rb
+++ cookbooks/camus/attributes/default.rb
@@ -7,10 +7,5 @@ default[:camus][:topics] = nil
 default[:camus][:etl][:destination] = "/history/#{node.cluster_name}"
 default[:camus][:etl][:base_path] = "/camus/#{node.cluster_name}/running"
 default[:camus][:etl][:history_path] = "/camus/#{node.cluster_name}/state"
-default[:camus][:etl][:writer] = "com.linkedin.camus.etl.kafka.common.JSONRecordWriterProvider"
-
-default[:camus][:message][:decoder] = "com.linkedin.camus.etl.kafka.coders.JsonStringMessageDecoder"
-default[:camus][:message][:timestamp][:field] = "ts"
-default[:camus][:message][:timestamp][:format] = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 
 default[:camus][:hadoop][:path] = "/var/app/hadoop2/current/bin"
