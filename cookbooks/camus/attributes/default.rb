0188925c709969949a0835a956825813a6c4a2ce
Benedikt Böhm <bb@xnull.de>
2014-09-24 15:17:33 +0200

    camus: hardcode hadoop path

--- cookbooks/camus/attributes/default.rb
+++ cookbooks/camus/attributes/default.rb
@@ -1,11 +1,8 @@
 default[:camus][:git][:repository] = "https://github.com/linkedin/camus"
 default[:camus][:git][:revision] = "master"
 
-default[:camus][:cluster] = node.cluster_name
 default[:camus][:topics] = nil
 
 default[:camus][:etl][:destination] = "/history/#{node.cluster_name}"
 default[:camus][:etl][:base_path] = "/camus/#{node.cluster_name}/running"
 default[:camus][:etl][:history_path] = "/camus/#{node.cluster_name}/state"
-
-default[:camus][:hadoop][:path] = "/var/app/hadoop2/current/bin"
--- cookbooks/camus/templates/default/camus.sh
+++ cookbooks/camus/templates/default/camus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CLASSPATH="$(echo /var/app/camus/current/target/camus-*-SNAPSHOT-shaded.jar):$(<%= node[:camus][:hadoop][:path]%>/hadoop classpath)"
+CLASSPATH="$(echo /var/app/camus/current/target/camus-*-SNAPSHOT-shaded.jar):$(/var/app/hadoop2/current/bin/hadoop classpath)"
 exec /usr/bin/java -cp $CLASSPATH com.linkedin.camus.etl.kafka.CamusJob -P /etc/camus/camus.properties
