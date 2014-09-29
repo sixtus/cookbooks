b5e7f93c60e87fd824185736a5de9bdf3828936e
Martin Karlsch <martin@karlsch.com>
2014-09-29 11:36:07 +0200

    update pig to 0.13

--- cookbooks/hadoop2/attributes/default.rb
+++ cookbooks/hadoop2/attributes/default.rb
@@ -32,13 +32,12 @@ default[:hadoop2][:yarn][:mem_per_container] = [
   mem_total / node[:hadoop2][:yarn][:containers],
 ].max
 
-default[:hadoop2][:pig][:version] = "0.12.1"
+default[:hadoop2][:pig][:version] = "0.13.0"
 default[:hadoop2][:pig][:default_jars] = %w{
   http://search.maven.org/remotecontent?filepath=com/googlecode/json-simple/json-simple/1.1.1/json-simple-1.1.1.jar
-  http://search.maven.org/remotecontent?filepath=com/twitter/elephantbird/elephant-bird-pig/4.4/elephant-bird-pig-4.4.jar
-  http://search.maven.org/remotecontent?filepath=com/twitter/elephantbird/elephant-bird-hadoop-compat/4.4/elephant-bird-hadoop-compat-4.4.jar
-  http://search.maven.org/remotecontent?filepath=com/twitter/parquet-pig-bundle/1.4.1/parquet-pig-bundle-1.4.1.jar
-  http://search.maven.org/remotecontent?filepath=org/apache/pig/piggybank/0.12.0/piggybank-0.12.0.jar
+  http://search.maven.org/remotecontent?filepath=com/twitter/elephantbird/elephant-bird-pig/4.5/elephant-bird-pig-4.5.jar
+  http://search.maven.org/remotecontent?filepath=com/twitter/elephantbird/elephant-bird-hadoop-compat/4.5/elephant-bird-hadoop-compat-4.5.jar
+  http://search.maven.org/remotecontent?filepath=org/apache/pig/piggybank/0.13.0/piggybank-0.13.0.jar
   http://search.maven.org/remotecontent?filepath=mysql/mysql-connector-java/5.1.29/mysql-connector-java-5.1.29.jar
 }
 
