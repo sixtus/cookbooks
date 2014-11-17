e1638e6a79fb9c7f88275ccc4975fbc08df6805b
Benedikt Böhm <bb@xnull.de>
2014-11-17 09:36:57 +0100

    linux: add OVH nameservers

--- cookbooks/linux/attributes/resolv.rb
+++ cookbooks/linux/attributes/resolv.rb
@@ -1,9 +1,11 @@
+default[:resolv][:nameservers] = %w(4.2.2.2 8.8.8.8)
+
 %x(whois #{node[:ipaddress]} | grep -q HETZNER-RZ)
-if $?.exitstatus == 0
-  default[:resolv][:nameservers] = %w(213.133.98.98 213.133.99.99)
-else
-  default[:resolv][:nameservers] = %w(4.2.2.2 8.8.8.8)
-end
+default[:resolv][:nameservers] = %w(213.133.98.98 213.133.99.99) if $?.exitstatus == 0
+
+%x(whois #{node[:ipaddress]} | grep -q OVH)
+default[:resolv][:nameservers] = %w(213.186.40.81 213.186.33.99) if $?.exitstatus == 0
+
 default[:resolv][:search] = []
 default[:resolv][:hosts] = []
 default[:resolv][:aliases] = []
