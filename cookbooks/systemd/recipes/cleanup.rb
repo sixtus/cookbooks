45b677077ae90282202c60c9f3d7250ca58abdcc
Benedikt Böhm <bb@xnull.de>
2014-12-02 16:02:27 +0100

    systemd: fix regressions from systemd-217

--- cookbooks/systemd/recipes/cleanup.rb
+++ cookbooks/systemd/recipes/cleanup.rb
@@ -1,9 +1,36 @@
 # cleanup cruft from SysV init & friends
 %w(
+  dcron
+  syslog-ng
+).each do |s|
+  service s do
+    action [:disable, :stop]
+  end
+end
+
+service "network.service" do
+  action :disable
+  provider Chef::Provider::Service::Systemd
+end
+
+%w(
+  dcron
+  network
+  syslog-ng
+).each do |u|
+  systemd_unit u do
+    action :delete
+  end
+end
+
+%w(
   /etc/crontab
   /etc/ifdown
   /etc/ifup
   /etc/inittab
+  /etc/systemd/system/multi-user.target.wants/dcron.service
+  /etc/systemd/system/multi-user.target.wants/syslog-ng.service
+  /etc/systemd/system/syslog.service
 ).each do |f|
   file f do
     action :delete
@@ -29,26 +56,3 @@ end
   end
 end
 
-%w(
-  dcron
-  syslog-ng
-).each do |s|
-  service s do
-    action [:disable, :stop]
-  end
-end
-
-service "network.service" do
-  action :disable
-  provider Chef::Provider::Service::Systemd
-end
-
-%w(
-  dcron
-  network
-  syslog-ng
-).each do |u|
-  systemd_unit u do
-    action :delete
-  end
-end
