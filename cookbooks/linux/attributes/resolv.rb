93a3a4cdbda8725f87efb730a04f5f5838432ba3
Benedikt Böhm <bb@xnull.de>
2014-12-12 09:48:23 +0100

    linux: do not use slow and broken OVH nameservers

--- cookbooks/linux/attributes/resolv.rb
+++ cookbooks/linux/attributes/resolv.rb
@@ -1,10 +1,7 @@
-default[:resolv][:nameservers] = %w(4.2.2.2 8.8.8.8)
+default[:resolv][:nameservers] = %w(8.8.8.8 4.2.2.2)
 
 %x(whois #{node[:ipaddress]} | grep -q HETZNER-RZ)
 default[:resolv][:nameservers] = %w(213.133.98.98 213.133.99.99) if $?.exitstatus == 0
-
-%x(whois #{node[:ipaddress]} | grep -q OVH)
-default[:resolv][:nameservers] = %w(213.186.40.81 213.186.33.99) if $?.exitstatus == 0
 
 default[:resolv][:search] = []
 default[:resolv][:hosts] = []
