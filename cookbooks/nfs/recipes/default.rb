0ff7ffa06e1b35b1d6686011ea092a9dd0ffdaa2
Benedikt Böhm <bb@xnull.de>
2014-12-22 15:31:56 +0100

    nfs: add custom service units

--- cookbooks/nfs/recipes/default.rb
+++ cookbooks/nfs/recipes/default.rb
@@ -4,9 +4,13 @@ service "rpcbind.service" do
   action [:enable, :start]
 end
 
+systemd_unit "rpc-mountd.service"
+
 service "rpc-mountd.service" do
   action [:enable, :start]
 end
+
+systemd_unit "rpc-statd.service"
 
 service "rpc-statd.service" do
   action [:enable, :start]
