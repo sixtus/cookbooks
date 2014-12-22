4c9232f5e63dec91e8b32fc18ccbe4bf9298418a
Benedikt Böhm <bb@xnull.de>
2014-12-22 15:32:07 +0100

    rvm: upgrade to 1.26.5

--- cookbooks/rvm/providers/instance.rb
+++ cookbooks/rvm/providers/instance.rb
@@ -23,6 +23,8 @@ action :create do
     export USER=#{rvm[:user]}
     export HOME=#{rvm[:homedir]}
 
+    gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
     tmpfile=$(mktemp)
     curl -s -L -k https://get.rvm.io -o ${tmpfile}
     chmod +x ${tmpfile}
--- cookbooks/rvm/resources/instance.rb
+++ cookbooks/rvm/resources/instance.rb
@@ -1,5 +1,5 @@
 actions :create, :delete
 default_action :create
 
-attribute :version, kind_of: String, default: "1.25.27"
+attribute :version, kind_of: String, default: "1.26.5"
 attribute :update, kind_of: [FalseClass, TrueClass], default: false
