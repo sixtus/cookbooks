cfe8a29198539bc171364791adc2826e40bb05f8
Benedikt Böhm <bb@xnull.de>
2015-01-10 13:40:31 +0100

    linux: cofnigure package managers before systemd

--- cookbooks/linux/recipes/default.rb
+++ cookbooks/linux/recipes/default.rb
@@ -2,9 +2,9 @@ if root?
   include_recipe "linux::baselayout"
   include_recipe "linux::locales"
   include_recipe "linux::resolv"
+  include_recipe "linux::#{node[:platform]}"
   include_recipe "systemd"
   include_recipe "linux::sysctl"
-  include_recipe "linux::#{node[:platform]}"
 end
 
 include_recipe "linux::packages"
