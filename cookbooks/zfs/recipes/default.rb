580ab53553dd07130899235afb36e2966d892753
Benedikt Böhm <bb@xnull.de>
2015-01-10 13:40:10 +0100

    zfs: use setup instructions from quickstart

--- cookbooks/zfs/recipes/default.rb
+++ cookbooks/zfs/recipes/default.rb
@@ -3,3 +3,13 @@ package "sys-fs/zfs"
 service "zfs" do
   action [:enable, :start]
 end
+
+# check if quickstart has left us zpool setup instructions
+bash "zpool-create" do
+  code "set -e; source /zpool-create.sh"
+  only_if { File.exist?("/zpool-create.sh") }
+end
+
+file "/zpool-create.sh" do
+  action :delete
+end
