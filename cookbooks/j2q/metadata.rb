ac45e499324290f8a438e79e6fbb6323f3bab31b
Benedikt Böhm <bb@xnull.de>
2015-01-22 13:00:22 +0100

    add j2q cookbook

---
+++ cookbooks/j2q/metadata.rb
@@ -0,0 +1,10 @@
+description "systemd journal to kafka"
+
+maintainer "Benedikt Böhm"
+maintainer_email "bb@xnull.de"
+license "Apache v2.0"
+
+supports "gentoo"
+
+depends "golang"
+depends "kafka"
