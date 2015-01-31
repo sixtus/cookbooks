45b21c196548eef7904aa316bc570fdc686983dc
Benedikt Böhm <bb@xnull.de>
2015-01-31 23:28:28 +0100

    nepal: add mod_pagespeed, xcache and xdebug

--- cookbooks/nepal/recipes/web.rb
+++ cookbooks/nepal/recipes/web.rb
@@ -1,8 +1,4 @@
-directory "/srv/system/etc/apache" do
-  owner "root"
-  group "root"
-  mode "0755"
-end
+# PHP
 
 directory "/srv/system/etc/php" do
   owner "root"
@@ -47,6 +43,32 @@ if %w(5.3 5.4).include?(node[:php][:slot])
   end
 end
 
+package "dev-php/xcache"
+
+php_extension "xcache" do
+  template "web/xcache.ini"
+end
+
+package "dev-php/xdebug"
+
+directory "/var/tmp/php/xdebug" do
+  owner "root"
+  group "root"
+  mode "1777"
+end
+
+php_extension "xdebug" do
+  template "web/xdebug.ini"
+end
+
+# Apache
+
+directory "/srv/system/etc/apache" do
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
 include_recipe "apache::fastcgi"
 include_recipe "apache::wsgi"
 
@@ -59,6 +81,17 @@ package "www-apache/mod_security"
 
 apache_module "79_modsecurity" do
   template "web/79_mod_security.conf"
+end
+
+package "www-apache/mod_pagespeed"
+
+htpasswd_from_users "/etc/apache2/modules.d/80_mod_pagespeed.passwd" do
+  query ->(user) { user[:tags] && user[:tags].include?("hostmaster") }
+  owner "apache"
+end
+
+apache_module "80_mod_pagespeed" do
+  template "web/80_mod_pagespeed.conf"
 end
 
 ssl_certificate "/srv/system/etc/apache/server" do
