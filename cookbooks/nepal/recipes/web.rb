eac53f3aa0109c9acba2969db0faa8cfd8874e23
Benedikt Böhm <bb@xnull.de>
2015-03-11 17:32:37 +0100

    nepal: remove duplicate resources

--- cookbooks/nepal/recipes/web.rb
+++ cookbooks/nepal/recipes/web.rb
@@ -69,32 +69,6 @@ directory "/srv/system/etc/apache" do
   mode "0755"
 end
 
-package "dev-php/xcache"
-
-php_extension "xcache" do
-  template "web/xcache.ini"
-end
-
-package "dev-php/xdebug"
-
-directory "/var/tmp/php/xdebug" do
-  owner "root"
-  group "root"
-  mode "1777"
-end
-
-php_extension "xdebug" do
-  template "web/xdebug.ini"
-end
-
-# Apache
-
-directory "/srv/system/etc/apache" do
-  owner "root"
-  group "root"
-  mode "0755"
-end
-
 include_recipe "apache::fastcgi"
 include_recipe "apache::wsgi"
 
