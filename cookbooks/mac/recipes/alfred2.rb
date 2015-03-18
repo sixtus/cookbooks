5ea135b15123882bf553f3fb3df8dfbd90c178fa
Benedikt Böhm <bb@xnull.de>
2015-03-18 20:43:48 +0100

    mac: update alfred2

--- cookbooks/mac/recipes/alfred2.rb
+++ cookbooks/mac/recipes/alfred2.rb
@@ -1,6 +1,6 @@
 if mac_os_x?
   mac_package "Alfred 2" do
-    source "https://cachefly.alfredapp.com/Alfred_2.5_299.zip"
+    source "https://cachefly.alfredapp.com/Alfred_2.6_374.zip"
     type "zip_app"
   end
 end
