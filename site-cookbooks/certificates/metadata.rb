43f6765ca62ca22e5df46a2cfc6dc28513e74579
Benedikt Böhm <bb@xnull.de>
2015-03-18 20:44:44 +0100

    certificates: add metadata

---
+++ site-cookbooks/certificates/metadata.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+name "certificates"
+description "SSL Certificates"
+
+maintainer "Benedikt Böhm"
+maintainer_email "bb@xnull.de"
+license "Apache v2.0"
+
+supports "debian"
+supports "gentoo"
+supports "mac_os_x"
+supports "ubuntu"
