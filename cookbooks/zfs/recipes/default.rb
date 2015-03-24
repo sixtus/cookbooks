330bc81d6f65b2d2b3a738bee2b05c23807ec149
Hagen Rother <hagen.rother@liquidm.com>
2015-03-24 14:13:38 +0100

    zfs: rewrite init ftw and add nagios and scrubbing

--- cookbooks/zfs/recipes/default.rb
+++ cookbooks/zfs/recipes/default.rb
@@ -1,15 +1,21 @@
-package "sys-fs/zfs"
+if gentoo?
+  package "sys-fs/zfs"
 
-service "zfs" do
-  action [:enable, :start]
-end
+  systemd_unit "zfs.service" do
+    template true
+  end
 
-# check if quickstart has left us zpool setup instructions
-bash "zpool-create" do
-  code "set -e; source /zpool-create.sh"
-  only_if { File.exist?("/zpool-create.sh") }
-end
+  service "zfs" do
+    action [:enable, :start]
+  end
 
-file "/zpool-create.sh" do
-  action :delete
+  # check if quickstart has left us zpool setup instructions
+  bash "zpool-create" do
+    code "set -e; source /zpool-create.sh"
+    only_if { File.exist?("/zpool-create.sh") }
+  end
+
+  file "/zpool-create.sh" do
+    action :delete
+  end
 end
