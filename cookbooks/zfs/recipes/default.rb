20271ce9894b8efd34ee411628e93ef225dcb4e5
Benedikt Böhm <bb@xnull.de>
2015-03-26 08:25:58 +0100

    zfs: move nagios checks to default recipe

--- cookbooks/zfs/recipes/default.rb
+++ cookbooks/zfs/recipes/default.rb
@@ -19,3 +19,47 @@ end
 file "/zpool-create.sh" do
   action :delete
 end
+
+zfs_pools = %x{/sbin/zpool list -Ho name}.split("\n") rescue []
+
+if zfs_pools.any?
+  include_recipe "zfs"
+
+  zfs_pools.each do |name|
+    systemd_timer "zfs-scrub-#{name}" do
+      schedule %W(OnCalendar=weekly AccuracySec=1h)
+      unit command: "/sbin/zpool scrub #{name}"
+    end
+  end
+
+  if nagios_client?
+    cookbook_file "/usr/lib/nagios/plugins/check_zfs" do
+      source "check_zfs.pl"
+      owner "root"
+      group "root"
+      mode "0777"
+    end
+
+    sudo_rule "nagios-check_zfs" do
+      user "nagios"
+      runas "root"
+      command "NOPASSWD: /usr/lib/nagios/plugins/check_zfs"
+    end
+    sudo_rule "nagios-zpool" do
+      user "nagios"
+      runas "root"
+      command "NOPASSWD: /sbin/zpool"
+    end
+
+    zfs_pools.each do |name|
+      nrpe_command "check_zfs_#{name}" do
+        command "/usr/lib/nagios/plugins/check_zfs #{name} 1"
+      end unless name.nil?
+
+      nagios_service "ZFS-POOL-#{name.upcase}" do
+        check_command "check_nrpe!check_zfs_#{name}"
+        servicegroups "system"
+      end unless name.nil?
+    end
+  end
+end
