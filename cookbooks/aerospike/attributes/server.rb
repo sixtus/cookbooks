9de0aadd4d91cd402664cce8305c0e33b9f18e65
Benedikt Böhm <bb@xnull.de>
2015-04-19 11:34:54 +0200

    remove aerospike cookbook

--- cookbooks/aerospike/attributes/server.rb
+++
@@ -1,7 +0,0 @@
-default[:aerospike][:fd_max] = 50000
-default[:aerospike][:namespaces][:test] = {
-  replication_factor: 2,
-  memory_size: "16M",
-  default_ttl: "30d",
-  storage_engine: "memory",
-}
--- cookbooks/aerospike/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-package "dev-db/aerospike"
-
-if nagios_client?
-  nagios_plugin "check_aerospike"
-end
