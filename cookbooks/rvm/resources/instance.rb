b159a3a547fbe66d26b16ba99c2dab34412fd332
Benedikt Böhm <bb@xnull.de>
2015-05-01 12:04:55 +0200

    rvm: update to 1.26.11

--- cookbooks/rvm/resources/instance.rb
+++ cookbooks/rvm/resources/instance.rb
@@ -1,5 +1,5 @@
 actions :create, :delete
 default_action :create
 
-attribute :version, kind_of: String, default: "1.26.5"
+attribute :version, kind_of: String, default: "1.26.11"
 attribute :update, kind_of: [FalseClass, TrueClass], default: false
