ea1c73c90a45993053c5a7df80f0243fa583e938
Benedikt Böhm <bb@xnull.de>
2015-05-02 10:29:03 +0200

    add command support to sshlive helper

--- tasks/support/ssh.rb
+++ tasks/support/ssh.rb
@@ -3,8 +3,10 @@ def sshlive(host, password = nil, script = nil)
   key = File.join(ROOT, "tasks", "support", "id_rsa")
   File.chmod(0400, key) # for good measure
   ssh = %{#{prefix} ssh -l root -i #{key} -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -o "GlobalKnownHostsFile /dev/null"}
-  if script
+  if File.exist?(script)
     sh(%{cat #{script} | #{ssh} #{host} "bash -s"})
+  elsif script
+    sh(%{#{ssh} #{host} #{script}})
   else
     sh(%{#{ssh} #{host}})
   end
