224f81b644b27314fe7eab0a8457ea29a8b01728
Benedikt Böhm <bb@xnull.de>
2015-06-09 21:19:49 +0200

    postfix: increase message size limit

--- cookbooks/postfix/attributes/default.rb
+++ cookbooks/postfix/attributes/default.rb
@@ -2,7 +2,7 @@ default[:skip][:postfix_satelite] = false
 
 default[:postfix][:use_flags] = []
 
-default[:postfix][:message_size_limit] = 10
+default[:postfix][:message_size_limit] = 25
 default[:postfix][:mynetworks] = []
 default[:postfix][:rbl_servers] = %w(zen.spamhaus.org bl.spamcop.net)
 
