3ab46e054fb210bfd05320ee054de874c90cc65c
Benedikt Böhm <bb@xnull.de>
2015-07-04 20:48:59 +0200

    ohai: add private_ipaddress plugin

---
+++ cookbooks/ohai/files/default/plugins/private_ipaddress.rb
@@ -0,0 +1,22 @@
+Ohai.plugin(:PrivateIpaddress) do
+  provides "private_ipaddress"
+  provides "private_hostname"
+
+  depends "network"
+  depends "hostname"
+
+  collect_data(:default) do
+    Ohai::Log.debug("looking for a private network interface in #{network.inspect}")
+
+    ip = network['interfaces']
+      .map { |name, i| (i['addresses'] || {}).keys }
+      .flatten
+      .grep(/^10\.|^172\.1[6-9]\.|^172\.2\d\.|^172\.3[0-1]\.|^192\.168/)
+      .first
+
+    if ip
+      private_ipaddress(ip)
+      private_hostname("#{hostname}.local")
+    end
+  end
+end
