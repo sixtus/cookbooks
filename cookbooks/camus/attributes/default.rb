e113f10ddee333ed8aa65e821bfceb7844ac25b3
Benedikt Böhm <bb@xnull.de>
2015-07-25 21:39:58 +0200

    camus: use one runner/job per topic

--- cookbooks/camus/attributes/default.rb
+++ cookbooks/camus/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:camus][:git][:repository] = "https://github.com/remerge/camus"
 
 # no topics means all topics
-default[:camus][:topics] = []
+default[:camus][:topics] = {}
 
 default[:camus][:destination] = "/history/#{node.cluster_name}"
 default[:camus][:base_path] = "/camus/#{node.cluster_name}/running"
--- cookbooks/camus/recipes/default.rb
+++ cookbooks/camus/recipes/default.rb
@@ -1,3 +1,5 @@
+homedir = "/var/app/camus"
+
 include_recipe "java"
 
 deploy_skeleton "camus"
@@ -15,7 +17,7 @@ deploy_application "camus" do
   end
 end
 
-template "/var/app/camus/current/camus.properties" do
+template "#{homedir}/current/camus.properties" do
   source "camus.properties"
   owner "camus"
   group "camus"
@@ -23,14 +25,38 @@ template "/var/app/camus/current/camus.properties" do
 end
 
 systemd_unit "camus.service" do
-  template true
+  action :delete
+end
+
+systemd_timer "camus" do
+  action :delete
 end
 
 primary = (node[:fqdn] == camus_nodes.first[:fqdn]) rescue false
 
-systemd_timer "camus" do
-  schedule %w(OnCalendar=*:30)
-  action :delete unless primary
+unit_defaults = {
+  directory: "#{homedir}/current",
+  user: "camus",
+  group: "camus",
+}
+
+node[:camus][:topics].each do |name, properties|
+  args = (properties || {}).merge({
+    "kafka.whitelist.topics" => name,
+    "camus.job.name" => "camus-#{name}",
+    "etl.execution.base.path" => "#{node[:camus][:base_path]}/#{name}",
+    "etl.execution.history.path" => "#{node[:camus][:history_path]}/#{name}",
+  }).map do |key, value|
+    "-D#{key}=#{value}"
+  end.join(' ')
+
+  systemd_timer "camus-#{name}" do
+    schedule %w(OnCalendar=*:20 AccuracySec=20m)
+    action :delete unless primary
+    unit(unit_defaults.merge({
+      command: "/var/app/camus/current/camus #{args}",
+    }))
+  end
 end
 
 if nagios_client?
