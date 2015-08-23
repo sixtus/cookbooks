c2e7c2d29cdfb40c60888666129721875ce70752
Benedikt Böhm <bb@xnull.de>
2015-08-23 20:11:03 +0200

    prefer hpssacli for check_raid

--- cookbooks/hwraid/recipes/hpsa.rb
+++ cookbooks/hwraid/recipes/hpsa.rb
@@ -13,3 +13,19 @@ sudo_rule "nagios-hpacucli-logical" do
   command "NOPASSWD: /usr/sbin/hpacucli controller * logicaldrive all show"
   only_if { nagios_client? }
 end
+
+package "sys-block/hpssacli"
+
+sudo_rule "nagios-hpssacli-controller" do
+  user "nagios"
+  runas "root"
+  command "NOPASSWD: /usr/sbin/hpssacli controller all show status"
+  only_if { nagios_client? }
+end
+
+sudo_rule "nagios-hpssacli-logical" do
+  user "nagios"
+  runas "root"
+  command "NOPASSWD: /usr/sbin/hpssacli controller * logicaldrive all show"
+  only_if { nagios_client? }
+end
