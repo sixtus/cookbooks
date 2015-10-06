190bee777961bca31dfaf6ae5a39d2445c3ae9e2
Benedikt Böhm <bb@xnull.de>
2015-10-06 21:14:03 +0200

    rollbar: do not check return code

--- cookbooks/rollbar/providers/deployment.rb
+++ cookbooks/rollbar/providers/deployment.rb
@@ -47,10 +47,4 @@ action :notify do
     req.form_data = payload
     res = http.request(req)
   end
-
-  if res.code != "200"
-    body = res.body.length != 0 ? res.body : "<no body>"
-    Chef::Log.error("Invalid status code from rollbar #{res.code}: #{body}")
-    raise "Invalid status code from rollbar #{res.code}"
-  end
 end
