c29fff2902dd63ee1d362385ef161c6e633b6127
Benedikt Böhm <bb@xnull.de>
2015-10-28 11:32:00 +0100

    nfs: adapt for latest upstream changes

--- cookbooks/nfs/recipes/default.rb
+++ cookbooks/nfs/recipes/default.rb
@@ -4,12 +4,6 @@ service "rpcbind.service" do
   action [:enable, :start]
 end
 
-systemd_unit "rpc-mountd.service"
-
-service "rpc-mountd.service" do
-  action [:enable, :start]
-end
-
 systemd_unit "rpc-statd.service"
 
 service "rpc-statd.service" do
--- cookbooks/nfs/recipes/server.rb
+++ cookbooks/nfs/recipes/server.rb
@@ -1,6 +1,14 @@
 include_recipe "nfs"
 
-service "nfsd" do
+systemd_unit "nfs-mountd.service"
+
+service "nfs-mountd.service" do
+  action [:enable, :start]
+end
+
+systemd_unit "nfs-server.service"
+
+service "nfs-server" do
   action [:enable, :start]
 end
 
