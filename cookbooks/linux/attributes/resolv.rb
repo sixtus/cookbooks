bfbf5301bab12e761fd8b60f5d8400717086e113
Benedikt Böhm <bb@xnull.de>
2016-02-22 09:59:56 +0100

    linux: remove resolv.conf hack

--- cookbooks/linux/attributes/resolv.rb
+++ cookbooks/linux/attributes/resolv.rb
@@ -1,8 +1,4 @@
 default[:resolv][:nameservers] = %w(8.8.8.8 8.8.4.4)
-
-%x(whois #{node[:ipaddress]} | grep -q HETZNER-RZ)
-default[:resolv][:nameservers] = %w(213.133.98.98 213.133.99.99) if $?.exitstatus == 0
-
 default[:resolv][:search] = []
 default[:resolv][:hosts] = []
 default[:resolv][:aliases] = []
