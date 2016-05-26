e3927d4f0ea79982b5f41b8b80fc1959e8e02f28
Benedikt Böhm <bb@xnull.de>
2016-05-26 10:53:12 +0200

    hadoop2: mirror ourselves, since apache is down often

--- cookbooks/hadoop2/recipes/pig.rb
+++ cookbooks/hadoop2/recipes/pig.rb
@@ -11,13 +11,13 @@ include_recipe "hadoop2"
   end
 end
 
-pig_tar = "https://archive.apache.org/dist/pig/pig-#{node[:hadoop2][:pig][:version]}/pig-#{node[:hadoop2][:pig][:version]}-src.tar.gz"
+pig_tar = "https://d2ljt3w7wnnuw2.cloudfront.net/pig-#{node[:hadoop2][:pig][:version]}-src.tar.gz"
 pig_basedir = "/var/app/hadoop2/pig"
 pig_dir = "#{pig_basedir}/pig-#{node[:hadoop2][:pig][:version]}-src"
 
 # bug fix, remove when ant runs w/o it (or dependencies change)
 remote_file "/var/app/hadoop2/.m2/repository/org/mortbay/jetty/jetty/6.1.26/jetty-6.1.26.zip" do
-  source "http://zenops-assets.s3.amazonaws.com/jetty-6.1.26-bundle.zip"
+  source "https://d2ljt3w7wnnuw2.cloudfront.net/jetty-6.1.26-bundle.zip"
   user "hadoop2"
   group "hadoop2"
 end
