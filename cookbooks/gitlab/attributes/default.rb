5325de1981129fb7b6ab7263946d7b970b475aa8
Benedikt Böhm <bb@xnull.de>
2016-05-28 21:21:06 +0200

    remove old gitlab cookbook

--- cookbooks/gitlab/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-default[:gitlab][:server_name] = "lab.#{node[:chef_domain]}"
-default[:gitlab][:certificate] = "wildcard.#{node[:chef_domain]}"
-
-default[:gitlab][:email_from] = node[:contacts][:hostmaster]
-default[:gitlab][:support_email] = node[:contacts][:hostmaster]
-
-# unicorn config
-default[:gitlab][:unicorn][:worker_processes] = 4
-default[:gitlab][:unicorn][:timeout] = 30
